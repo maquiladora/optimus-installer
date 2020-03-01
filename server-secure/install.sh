6248da9d2fe4500e85dacee119a9ad36a2f3a1b2
MetallianFR68 <lionel.vest@adaris.org>
2020-03-01 20:46:54 +0100

    update

--- server-secure/install.sh
+++ server-secure/install.sh
@@ -18,3 +18,25 @@ then
 fi
 
 echo
+read -p "Voulez vous créer un utilisateur secondaire dénommé optimus ? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[YyOo]$ ]]
+then
+    adduser optimus
+fi
+
+echo
+read -p "Voulez vous interdire l'accès SSH à l'utilisateur root ? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[YyOo]$ ]]
+then
+  if [ $(getent passwd optimus) ]
+  then
+    sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
+    service sshd restart
+  else
+    echo "L'accès SSD de l'utilisateur root ne peut pas être désactivé si l'utilisateur optimus n'a pas été créé"
+  fi
+fi
+
+echo
