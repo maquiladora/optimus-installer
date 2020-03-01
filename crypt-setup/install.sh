e070251d965f9b1f1482f037354b0c1339d3fb10
MetallianFR68 <lionel.vest@adaris.org>
2020-03-01 23:13:13 +0100

    update

--- crypt-setup/install.sh
+++ crypt-setup/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get -qq -y install cryptsetup
+apt-get -qq -y install cryptsetup cryptsetup-bin
 cryptsetup luksFormat /dev/sda2
 cryptsetup luksOpen /dev/sda2 cryptsda2
 mkfs.ext4 /dev/mapper/cryptsda2
