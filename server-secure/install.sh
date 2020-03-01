40620422c5a73999b5bd7a3954ceaeb86f17e493
MetallianFR68 <lionel.vest@adaris.org>
2020-03-01 18:14:46 +0100

    update

--- server-secure/install.sh
+++ server-secure/install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-read -p "\nVoulez vous modifier le mot de passe root ? " -n 1 -r
+echo
+read -p "Voulez vous modifier le mot de passe root ? " -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
     passwd
 fi
 
-read -p "\nVoulez vous modifier le port de connexion SSH par défaut ? " -n 1 -r
+echo
+read -p "Voulez vous modifier le port de connexion SSH par défaut ? " -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
     echo "hopla"
