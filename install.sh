df18955ddbce494e911cd801dd36244092e103de
MetallianFR68 <lionel.vest@adaris.org>
2020-03-01 13:17:15 +0100

    update

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 apt-get update
 apt-get -qq -y install git
+cd /
 rm -R /installer
 mkdir /installer
 git clone -b vest https://github.com/MetallianFR68/optimus-installer /installer
+cd /installer
 chmod +x menu.sh
-source /installer/menu.sh
+source menu.sh
