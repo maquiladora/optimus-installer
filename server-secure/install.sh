3c607a4fb6c4229ecf3672ede9cae88ea2d13642
MetallianFR68 <lionel.vest@adaris.org>
2020-03-01 18:22:45 +0100

    update

--- server-secure/install.sh
+++ server-secure/install.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 echo
-read -p "Voulez vous modifier le mot de passe root ?" -n 1 -r
+read -p "Voulez vous modifier le mot de passe root ? " -n 1 -r
 echo
-if [[ $REPLY =~ ^[Yy]$ ]]
+if [[ $REPLY =~ ^[YyOo]$ ]]
 then
     passwd
 fi
 
 echo
-read -p "Voulez vous modifier le port de connexion SSH par défaut ?" -n 1 -r
+read -p "Voulez vous remplacer le port de connexion SSH par le port 7822 ? " -n 1 -r
 echo
-if [[ $REPLY =~ ^[Yy]$ ]]
+if [[ $REPLY =~ ^[YyOo]$ ]]
 then
     echo "hopla"
+    sed -i 's/Port 22/Port 7822/g' /etc/ssh/sshd_config
+    service sshd restart
 fi
 
 echo
