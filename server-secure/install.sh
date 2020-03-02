9e919eb368eeaa1f518ea989854934cc6e4f3130
MetallianFR68 <lionel.vest@adaris.org>
2020-03-02 16:21:23 +0100

    update

--- server-secure/install.sh
+++ server-secure/install.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 echo
-read -p $'\e[32mVoulez vous mettre à jour le système -> update/upgrade (o/n) ?\e[0m' -n 1 -r
+read -p $'\e[32mVoulez vous mettre à jour le système -> update/upgrade (o/n) ? \e[0m' -n 1 -r
 echo
 if [[ $REPLY =~ ^[YyOo]$ ]]
 then
   apt-get -qq -y update
-  apt-get -qq -y upgrade
+  apt-get -y upgrade
 fi
 
 echo
-read -p $'\e[32mVoulez vous modifier le mot de passe root (o/n) ?\e[0m' -n 1 -r
+read -p $'\e[32mVoulez vous modifier le mot de passe root (o/n) ? \e[0m' -n 1 -r
 echo
 if [[ $REPLY =~ ^[YyOo]$ ]]
 then
-  read -p $'\e[32mVoulez vous générer un mot de passe automatiquement (o/n) ?\e[0m' -n 1 -r
+  read -p $'\e[32mVoulez vous générer un mot de passe automatiquement (o/n) ? \e[0m' -n 1 -r
   echo
   if [[ $REPLY =~ ^[YyOo]$ ]]
   then
@@ -30,7 +30,7 @@ fi
 if [ ! -d "/home/optimus" ]
 then
   echo
-  read -p $'\e[32mVoulez vous créer un utilisateur secondaire dénommé optimus (o/n) ?\e[0m' -n 1 -r
+  read -p $'\e[32mVoulez vous créer un utilisateur secondaire dénommé optimus (o/n) ? \e[0m' -n 1 -r
   echo
   if [[ $REPLY =~ ^[YyOo]$ ]]
   then
@@ -39,12 +39,12 @@ then
       then
         cp /root/.google_authenticator /home/optimus/.google_authenticator
       fi
-      read -p $'\e[32mVoulez vous générer un mot de passe automatiquement (o/n) ?\e[0m' -n 1 -r
+      read -p $'\e[32mVoulez vous générer un mot de passe automatiquement (o/n) ? \e[0m' -n 1 -r
       echo
       if [[ $REPLY =~ ^[YyOo]$ ]]
       then
         newoptimuspass=$(</dev/urandom tr -dc A-Za-z0-9 | head -c${1:-32})
-        echo -e "$newoptimuspass\n$newoptimuspass" | passwd optimus
+        echo -e "$newoptimuspass\n$newoptimuspass" | passwd optimus &> /dev/null
         echo "Le nouveau mot de passe de l'utilisateur optimus est : $newoptimuspass"
       else
         passwd
@@ -54,18 +54,7 @@ fi
 
 
 echo
-read -p $'\e[32mVoulez vous remplacer le port de connexion SSH par le port 7822 (o/n) ?\e[0m' -n 1 -r
-echo
-if [[ $REPLY =~ ^[YyOo]$ ]]
-then
-    sed -i 's/#Port 22/Port 7822/g' /etc/ssh/sshd_config
-    ufw allow 7822
-    systemctl restart ssh
-fi
-
-
-echo
-read -p $'\e[32mVoulez vous activer le firewall (o/n) ?\e[0m' -n 1 -r
+read -p $'\e[32mVoulez vous activer le firewall (o/n) ? \e[0m' -n 1 -r
 echo
 if [[ $REPLY =~ ^[YyOo]$ ]]
 then
@@ -77,11 +66,30 @@ then
     ufw allow 22
   fi
   ufw --force enable
+else
+  ufw --force disable
 fi
 
 
 echo
-read -p $'\e[32mVoulez vous interdire l'accès SSH à l'utilisateur root (o/n) ?\e[0m' -n 1 -r
+read -p $'\e[32mVoulez vous remplacer le port de connexion SSH par le port 7822 (o/n) ? \e[0m' -n 1 -r
+echo
+if [[ $REPLY =~ ^[YyOo]$ ]]
+then
+  sed -i 's/#Port 22/Port 7822/g' /etc/ssh/sshd_config
+  ufw allow 7822
+  ufw deny 22
+  systemctl restart ssh
+else
+  sed -i 's/Port 7822/#Port 22/g' /etc/ssh/sshd_config
+  ufw deny 7822
+  ufw allow 22
+  systemctl restart ssh
+fi
+
+
+echo
+read -p $'\e[32mVoulez vous interdire l\'accès SSH à l\'utilisateur root (o/n) ? \e[0m' -n 1 -r
 echo
 if [[ $REPLY =~ ^[YyOo]$ ]]
 then
@@ -95,11 +103,12 @@ then
 fi
 
 echo
-read -p $'\e[32mVoulez vous installer FAIL2BAN (o/n) ?\e[0m' -n 1 -r
+read -p $'\e[32mVoulez vous installer FAIL2BAN (o/n) ? \e[0m' -n 1 -r
 echo
 if [[ $REPLY =~ ^[YyOo]$ ]]
 then
   apt-get -qq -y install fail2ban
   fail2ban-client status
 fi
+
 echo
