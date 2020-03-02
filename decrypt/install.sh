a3db2722ac03e9e6ea3957de0a2e67476e9342fa
MetallianFR68 <lionel.vest@adaris.org>
2020-03-03 00:24:45 +0100

    update

--- decrypt/install.sh
+++ decrypt/install.sh
@@ -2,7 +2,7 @@
 
 if [ -f /root/keyfile ]
 then
-  /sbin/cryptsetup luksOpen /dev/sda2 cryptsda2 --key-file /root/keyfile
+  /sbin/cryptsetup luksOpen /dev/sda2 cryptsda2 --key-file < openssl rsautl -decrypt -inkey /root/private.pem -in /root/keyfile_encrypted
 else
   /sbin/cryptsetup luksOpen /dev/sda2 cryptsda2
 fi
