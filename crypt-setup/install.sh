424dbe74312471774d5c35db64d2ed3ce8b61ca7
MetallianFR68 <lionel.vest@adaris.org>
2020-03-02 23:35:18 +0100

    update

--- crypt-setup/install.sh
+++ crypt-setup/install.sh
@@ -9,12 +9,16 @@ then
   dd if=/dev/urandom of=/root/keyfile bs=1024 count=4 > /dev/null
   chmod 0400 /root/keyfile
   #cryptsetup luksAddKey /dev/sda2 /root/keyfile
+  openssl genrsa -out /root/private.pem 2048
+  openssl rsa -in /root/private.pem -outform PEM -pubout -out /root/public.pem
+  openssl rsautl -encrypt -inkey /root/public.pem -pubin -in /root/keyfile -out /root/keyfile_encrypted
   sleep 0.5
   echo -e "\e[35mACTIVATION DU CRYPTAGE SUR LA PARTITION /dev/sda2...\e[0m"
   /sbin/cryptsetup --batch-mode luksFormat /dev/sda2 /root/keyfile
   sleep 0.5
   echo -e "\e[35mOUVERTURE DE LA PARTITION CRYPTEE...\e[0m"
-  /sbin/cryptsetup luksOpen /dev/sda2 cryptsda2 --key-file /root/keyfile
+  openssl rsautl -decrypt -inkey /root/private.pem -in /root/keyfile_encrypted -out /root/keyfile_decrypted
+  /sbin/cryptsetup luksOpen /dev/sda2 cryptsda2 --key-file /root/keyfile_decrypted
   sleep 0.5
   echo -e "\e[35mFORMATAGE DE LA PARTITION CRYPTEE EN EXT4...\e[0m"
   /sbin/mkfs.ext4 /dev/mapper/cryptsda2 > /dev/null
