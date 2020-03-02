cff786404ad5ebd579bc305c1c66643fb4b3654e
MetallianFR68 <lionel.vest@adaris.org>
2020-03-02 17:27:59 +0100

    update

--- diskpart/install.sh
+++ diskpart/install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo -e "\e[31m!! ATTENTION !!\e[0m"
+echo -e "\e[31mCETTE OPERATION ET RISQUEE ET PEUT CORROMPRE LE DISQUE ET LE SYSTEME\e[0m"
+echo -e "\e[31mIL N'EST RECOMMANDE DE LA LANCER QUE SUR UN SYSTEME VIERGE QUI VIENT D'ETRE INSTALLE\e[0m"
+echo
+
 cp /installer/diskpart/resizefs_hook /etc/initramfs-tools/hooks/resizefs_hook
 chmod +x /etc/initramfs-tools/hooks/resizefs_hook
 
