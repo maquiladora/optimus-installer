af5cd2bbe91081608764ce05cfe1e23dfb581327
MetallianFR68 <lionel.vest@adaris.org>
2020-03-02 14:22:57 +0100

    Update

--- crypt-setup/install.sh
+++ crypt-setup/install.sh
@@ -2,7 +2,7 @@
 
 DEBIAN_FRONTEND=noninteractive apt-get -qq -y keyboard-configuration
 apt-get -qq -y install cryptsetup cryptsetup-bin
-/sbin/cryptsetup luksFormat /dev/sda2
+/sbin/cryptsetup --batch-mode luksFormat /dev/sda2
 /sbin/cryptsetup luksOpen /dev/sda2 cryptsda2
 /sbin/mkfs.ext4 /dev/mapper/cryptsda2
 /sbin/cryptsetup luksClose /dev/mapper/cryptsda2
