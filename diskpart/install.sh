ceb0b97529782f4626b51ab55540160b1fcbb4b3
MetallianFR68 <lionel.vest@adaris.org>
2020-03-02 21:36:32 +0100

    update

--- diskpart/install.sh
+++ diskpart/install.sh
@@ -10,24 +10,20 @@ read -p $'\e[32mETES VOUS SUR (o/n) ? \e[0m' -n 1 -r
 echo
 if [[ $REPLY =~ ^[YyOo]$ ]]
 then
-  echo -e "\e[35mCREATION DES SCRIPTS DE REPARTITIONNEMENT...\e[0m"
-
-  apt-get remove -qq cryptsetup-initramfs > /dev/null
-
+  echo -e "\e[35mMISE EN PLACE DES SCRIPTS DE REPARTITIONNEMENT...\e[0m"
   cp /installer/diskpart/resizefs_hook /etc/initramfs-tools/hooks/resizefs_hook
   chmod +x /etc/initramfs-tools/hooks/resizefs_hook
-
   cp /installer/diskpart/resizefs /etc/initramfs-tools/scripts/local-premount/resizefs
   chmod +x /etc/initramfs-tools/scripts/local-premount/resizefs
-
   cp /installer/diskpart/rc.local /etc/rc.local
   chmod +x /etc/rc.local
 
+  echo -e "\e[35mMISE A JOUR INITRAMFS...\e[0m"
+  apt-get remove -qq cryptsetup-initramfs > /dev/null
   update-initramfs -u > /dev/null
 
-  echo
   echo -e "\e[35mLE SERVEUR DOIT REDEMARRER POUR REDIMENSIONNER LES PARTITIONS\e[0m"
-  echo -e "\e[32mAPPUYER SUR [ENTREE] POUR REDEMARRER\e[0m"
+  echo -e "\e[32mAPPUYER SUR UNE TOUCHE POUR REDEMARRER\e[0m"
   read -n 1
   reboot
 fi
