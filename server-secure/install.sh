9312b87f6269f6f8fad429c1cdd2d4d4353fd115
MetallianFR68 <lionel.vest@adaris.org>
2020-03-02 15:38:17 +0100

    update

--- server-secure/install.sh
+++ server-secure/install.sh
@@ -18,32 +18,13 @@ then
   if [[ $REPLY =~ ^[YyOo]$ ]]
   then
     newrootpass=$(</dev/urandom tr -dc A-Za-z0-9 | head -c${1:-32})
-    echo "$newrootpass\n$newrootpass" | passwd root
+    echo -e "$newrootpass\n$newrootpass" | passwd root
     echo "Le nouveau mot de passe de l'utilisateur root est : $newrootpass"
   else
     passwd
   fi
 fi
 
-echo
-read -p "Voulez vous activer le firewall ? " -n 1 -r
-echo
-if [[ $REPLY =~ ^[YyOo]$ ]]
-then
-  apt-get -qq -y install ufw
-  ufw --force allow 22
-  ufw --force enable
-fi
-
-echo
-read -p "Voulez vous remplacer le port de connexion SSH par le port 7822 ? " -n 1 -r
-echo
-if [[ $REPLY =~ ^[YyOo]$ ]]
-then
-    sed -i 's/#Port 22/Port 7822/g' /etc/ssh/sshd_config
-    ufw allow 7822
-    systemctl restart ssh
-fi
 
 if [ ! -d "/home/optimus" ]
 then
@@ -61,13 +42,41 @@ then
       if [[ $REPLY =~ ^[YyOo]$ ]]
       then
         newoptimuspass=$(</dev/urandom tr -dc A-Za-z0-9 | head -c${1:-32})
-        echo "$newoptimuspass\n$newoptimuspass" | passwd optimus
+        echo -e "$newoptimuspass\n$newoptimuspass" | passwd optimus
         echo "Le nouveau mot de passe de l'utilisateur optimus est : $newoptimuspass"
       else
         passwd
       fi
   fi
 fi
+
+
+echo
+read -p "Voulez vous remplacer le port de connexion SSH par le port 7822 ? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[YyOo]$ ]]
+then
+    sed -i 's/#Port 22/Port 7822/g' /etc/ssh/sshd_config
+    ufw allow 7822
+    systemctl restart ssh
+fi
+
+
+echo
+read -p "Voulez vous activer le firewall ? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[YyOo]$ ]]
+then
+  apt-get -qq -y install ufw
+  if grep -q "Port 7822" /etc/ssh/sshd_config
+  then
+    ufw allow 7822
+  else
+    ufw allow 22
+  fi
+  ufw --force enable
+fi
+
 
 echo
 read -p "Voulez vous interdire l'accès SSH à l'utilisateur root ? " -n 1 -r
