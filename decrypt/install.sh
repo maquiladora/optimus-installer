e37fb6f5ce51d9c9e18914c784755a881fdc67ce
MetallianFR68 <lionel.vest@adaris.org>
2020-03-03 01:08:43 +0100

    update

--- decrypt/install.sh
+++ decrypt/install.sh
@@ -2,8 +2,10 @@
 
 if [ -f /root/keyfile_encrypted ]
 then
+  echo -e "\e[35mOUVERTURE DE LA PARTITION CRYPTEE...\e[0m"
   openssl rsautl -decrypt -inkey /root/private.pem -in /root/keyfile_encrypted | /sbin/cryptsetup luksOpen /dev/sda2 cryptsda2
 else
+  echo -e "\e[35mOUVERTURE DE LA PARTITION CRYPTEE...\e[0m"
   /sbin/cryptsetup luksOpen /dev/sda2 cryptsda2
 fi
 
