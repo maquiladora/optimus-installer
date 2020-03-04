8f81ccbf903e97a8124debf8b9ba5a460bd51f24
MetallianFR68 <lionel.vest@adaris.org>
2020-03-04 03:21:48 +0100

    update

--- decrypt/install.sh
+++ decrypt/install.sh
@@ -2,7 +2,7 @@
 
 mkdir /root/tmpramfs
 mount ramfs /root/tmpramfs/ -t ramfs
-wget -O /root/tmpramfs/keyfile_encrypted https://installer.optimus-avocats.fr/autodecryptor/$(</root/uid)_keyfile
+wget -qO /root/tmpramfs/keyfile_encrypted https://installer.optimus-avocats.fr/autodecryptor/$(</root/uid)_keyfile
 openssl rsautl -decrypt -inkey /root/private.pem -in /root/tmpramfs/keyfile_encrypted | /sbin/cryptsetup luksOpen /dev/sda2 cryptsda2
 umount /root/tmpramfs
 rmdir /root/tmpramfs
