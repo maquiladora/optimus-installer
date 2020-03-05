6e7a92e9b84fb779a712004e01a99c9ec83db9c5
MetallianFR68 <lionel.vest@adaris.org>
2020-03-05 01:53:19 +0100

    update

--- functions.sh
+++ functions.sh
@@ -15,7 +15,7 @@ verbose()
 )
 
 echo_red()(echo -e "\e[31m${1}\e[0m")
-echo_red_blink()(echo -e "\033[33;5m${1}\033[0m")
+echo_red_blink()(echo -e "\e[5m${1}\e[0m")
 echo_green()(echo -e "\e[32m${1}\e[0m")
 echo_yellow()(echo -e "\e[33m${1}\e[0m")
 echo_blue()(echo -e "\e[34m${1}\e[0m")
