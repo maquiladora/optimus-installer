a3cd789a36f53a7f196b1971390bfc6ba38fb6d1
MetallianFR68 <lionel.vest@adaris.org>
2020-08-01 23:05:14 +0200

    apache

--- apache/install.sh
+++ apache/install.sh
@@ -2,52 +2,14 @@
 source /installer/functions.sh
 source /installer/config.sh
 
-
 if [ ! $APACHE_AREYOUSURE ]; then echo_green "Souhaitez vous installer le serveur apache ?"; read -p "(o)ui / (n)on ? " -n 1 -e APACHE_AREYOUSURE; fi
 if [[ $APACHE_AREYOUSURE =~ ^[YyOo]$ ]]
 then
+  echo_magenta "Installation du serveur Apache en cours..."
   verbose apt-get -qq install apache2
-
-  if [ $(which /sbin/ufw) ]
-  then
-    verbose /sbin/ufw allow 80
-    verbose /sbin/ufw allow 443
-  fi
-
-  sed -i 's/DocumentRoot \/var\/www\/html/DocumentRoot \/srv\/website/g' /etc/apache2/sites-enabled/000-default.conf
-
-  if grep -q "<Directory /srv/website/>" "/etc/apache2/apache2.conf"
-  then
-    printf '<Directory /srv/website/>\n\tOptions Indexes FollowSymLinks\n\tAllowOverride None\n\tRequire all granted\n</Directory>\n\n' >> /etc/apache2/apache2.conf
-  fi
-
-  if [ ! -d "/srv/website" ];  then  verbose mkdir /srv/website; fi
-  if [ ! -f "/srv/website/index.html" ]; then touch /srv/website/index.html; fi
-
-  <Directory /srv/website/>
-        Options Indexes FollowSymLinks
-        AllowOverride None
-        Require all granted
-</Directory>
-
+  if [ $(which /sbin/ufw) ]; then verbose /sbin/ufw allow 80; fi
+  verbose rm /var/www/html/index.html
+  verbose touch /var/www/html/index.html
   verbose systemctl restart apache2
-fi
-
-
-if [ ! $APACHE_DEFAULSITE_AREYOUSURE ]; then echo_green "Voulez-vous installer les hébergements par défaut ?"; read -p "(o)ui / (n)on ? " -n 1 -e APACHE_DEFAULSITE_AREYOUSURE; fi
-if [[ $APACHE_DEFAULSITE_AREYOUSURE =~ ^[YyOo]$ ]]
-then
-
-  if [ ! $DOMAIN ]; then echo_green "Merci d'indiquer votre nom de domaine"; read DOMAIN; fi
-
-  if [ ! $APACHE_WWW_AREYOUSURE ]; then echo_green "Voulez-vous installer l'hébergement de www.$DOMAIN' ?"; read -p "(o)ui / (n)on ? " -n 1 -e APACHE_WWW_AREYOUSURE; fi
-  if [[ $APACHE_WWW_AREYOUSURE =~ ^[YyOo]$ ]]
-  then
-    echo_magenta "Création du site par défaut www.$DOMAIN"
-    if [ ! -d "/srv/website" ];  then  verbose mkdir /srv/website; fi
-    if [ ! -f "/srv/website/index.html" ]; then verbose cp /installer/apache/index.html /srv/website; fi
-    if [ ! -f "/etc/apache2/sites-enabled/www.$DOMAIN.conf" ]; then sed -e 's/$DOMAIN/'$DOMAIN'/g' /installer/apache/default_vhost > /etc/apache2/sites-enabled/www.$DOMAIN.conf; fi
-    verbose systemctl restart apache2
-  fi
-
+  echo_magenta "Le serveur Apache a été installé avec succès !"
 fi
---
+++ website/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+source /installer/functions.sh
+source /installer/config.sh
+
+if [ ! $DOMAIN ]; then echo_green "Merci d'indiquer votre nom de domaine"; read DOMAIN; fi
+
+if [ ! $APACHE_DEFAULSITE_AREYOUSURE ]; then echo_green "Voulez-vous installer l'espace d'hébergement www.$DOMAIN ?"; read -p "(o)ui / (n)on ? " -n 1 -e APACHE_DEFAULSITE_AREYOUSURE; fi
+if [[ $APACHE_DEFAULSITE_AREYOUSURE =~ ^[YyOo]$ ]]
+then
+  echo_magenta "Création de l'espace d'hébergement www.$DOMAIN..."
+
+  if [ ! -d "/srv/www.$DOMAIN" ];  then  verbose mkdir /srv/www.$DOMAIN; fi
+  if [ ! -f "/srv/www.$DOMAIN/index.html" ]; then verbose echo "HELLO WORLD !" > /srv/www.$DOMAIN/index.html; fi
+  if [ ! -f "/etc/apache2/sites-enabled/www.$DOMAIN.conf" ]; then sed -e 's/%DOMAIN%/$DOMAIN/g' /installer/apache/default_vhost > /etc/apache2/sites-enabled/www.$DOMAIN.conf; fi
+
+  if grep -q "<Directory /srv/www.$DOMAIN/>" "/etc/apache2/apache2.conf"
+  then
+    printf '<Directory /srv/www.$DOMAIN/>\n\tOptions Indexes FollowSymLinks\n\tAllowOverride None\n\tRequire all granted\n</Directory>\n\n' >> /etc/apache2/apache2.conf
+  fi
+
+  verbose systemctl restart apache2
+  echo_magenta "L'espace d'hébergement www.$DOMAIN a été créé avec succès dans /srv/www.$DOMAIN !"
+fi
