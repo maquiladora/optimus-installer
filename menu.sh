a862fcc2b8e16996391522b066ed44a09e7f035f
MetallianFR68 <lionel.vest@adaris.org>
2020-08-01 23:08:20 +0200

    apache

--- menu.sh
+++ menu.sh
@@ -15,6 +15,7 @@ tput cup 6  3; if /sbin/blkid /dev/sda2 2>/dev/null | grep -q 'crypto_LUKS'; the
 tput cup 7  3; if lsblk -o MOUNTPOINT -n /dev/mapper/cryptsda2 2>/dev/null | grep -q '/srv'; then echo -ne "\e[32m c. Decrypter la partition /dev/sda2 et la monter sur /srv \e[0m"; else echo -ne "\e[31m c. Decrypter la partition /dev/sda2 et la monter sur /srv \e[0m"; fi
 tput cup 8  3; if grep -q "Port 7822" /etc/ssh/sshd_config; then echo -ne "\e[32m d. Sécuriser le serveur \e[0m"; else echo -ne "\e[31m d. Sécuriser le serveur \e[0m"; fi
 tput cup 9  3; if [ -f "/etc/apache2/apache2.conf" ];	then echo -ne "\e[32m e. Installer le serveur web APACHE \e[0m"; else echo -ne "\e[31m e. Installer le serveur web APACHE \e[0m"; fi
+tput cup 10 3; if [ -d "/srv/www.demoptimus.fr" ];	then echo -ne "\e[32m f. Installer l'espace d'hébergement www \e[0m"; else echo -ne "\e[31m f. Installer l'espace d'hébergement www \e[0m"; fi
 #lsblk -o MOUNTPOINT -n /dev/sda2
 
 #tput cup 5 	3; if [ -f "/etc/srv" ]; 		                then echo -ne "\e[32m c. Create /srv partition \e[0m"; 		        else echo -ne "\e[31m c. Create /srv partitions \e[0m"; fi
