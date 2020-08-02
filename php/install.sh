5cac4484eecd730087e33169a9a2c9bcc264423a
MetallianFR68 <lionel.vest@adaris.org>
2020-08-02 14:04:54 +0200

    php

--- php/install.sh
+++ php/install.sh
@@ -7,7 +7,8 @@ if [[ $PHP_AREYOUSURE =~ ^[YyOo]$ ]]
 then
 
   echo_magenta "Installation de PHP en cours..."
-  verbose apt-get -qq install php php-mysql php-mcrypt php-imap php-xmlrpc php-curl
+  verbose apt-get -qq install php php-mysql php-imap php-xmlrpc php-curl
+  verbose systemctl restart apache2
   echo_magenta "PHP a été installé avec succès !"
 
 fi
