4a0e0f6f8681785e8c0077764d1a165da2e1af8a
MetallianFR68 <lionel.vest@adaris.org>
2020-08-02 13:27:16 +0200

    letsencrypt

---
+++ letsencrypt/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+source /installer/functions.sh
+source /installer/config.sh
+
+if [ ! $DOMAIN ]; then echo_green "Merci d'indiquer votre nom de domaine"; read DOMAIN; fi
+
+if [ ! $LETSENCRYPT_AREYOUSURE ]; then echo_green "Souhaitez vous installer les certificats SSL HTTPS ?"; read -p "(o)ui / (n)on ? " -n 1 -e LETSENCRYPT_AREYOUSURE; fi
+if [[ $LETSENCRYPT_AREYOUSURE =~ ^[YyOo]$ ]]
+then
+
+    echo_magenta "Installing de letsencrypt en cours..."
+    verbose apt-get -qq -y install letsencrypt python-certbot-apache
+
+    if [ -d "/srv/www.$DOMAIN" ]; then DOMAINS_TO_INSTALL="-d $DOMAIN -d www.$DOMAIN"; fi
+    if [ -d "/srv/cloud.$DOMAIN" ]; then DOMAINS_TO_INSTALL="${DOMAINS_TO_INSTALL} -d cloud.$DOMAIN"; fi
+    if [ -d "/srv/webmail.$DOMAIN" ]; then DOMAINS_TO_INSTALL="${DOMAINS_TO_INSTALL} -d webmail.$DOMAIN"; fi
+    if [ -d "/srv/api.$DOMAIN" ]; then DOMAINS_TO_INSTALL="${DOMAINS_TO_INSTALL} -d api.$DOMAIN"; fi
+
+    verbose certbot run -n --apache --agree-tos --email webmaster@$DOMAIN $DOMAINS_TO_INSTALL
+
+    echo_magenta "les certificats SSL ont été installés avec succès..."
+
+fi
