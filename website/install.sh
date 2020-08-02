cb38c9e6351b3af681b05966d3b2334c969f1317
MetallianFR68 <lionel.vest@adaris.org>
2020-08-02 11:59:55 +0200

    apache

--- website/install.sh
+++ website/install.sh
@@ -13,7 +13,7 @@ then
   if [ ! -f "/srv/www.$DOMAIN/index.html" ]; then verbose echo "HELLO WORLD !" > /srv/www.$DOMAIN/index.html; fi
   if [ ! -f "/etc/apache2/sites-enabled/www.$DOMAIN.conf" ]; then sed -e 's/%DOMAIN%/'$DOMAIN'/g' /installer/website/vhost > /etc/apache2/sites-enabled/www.$DOMAIN.conf; fi
 
-  if grep -q "<Directory /srv/www.$DOMAIN/>" "/etc/apache2/apache2.conf"
+  if [ ! grep -q "<Directory /srv/www.$DOMAIN/>" "/etc/apache2/apache2.conf" ]
   then
     printf "<Directory /srv/www.$DOMAIN/>\n\tOptions Indexes FollowSymLinks\n\tAllowOverride None\n\tRequire all granted\n</Directory>\n\n" >> /etc/apache2/apache2.conf
   fi
