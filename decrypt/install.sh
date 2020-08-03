72ef5d9daca85668417c356c9efb3cfc4681f01c
MetallianFR68 <lionel.vest@adaris.org>
2020-08-04 00:10:44 +0200

    *

--- decrypt/install.sh
+++ decrypt/install.sh
@@ -5,17 +5,22 @@ source /installer/config.sh
 mkdir /root/tmpramfs
 mount ramfs /root/tmpramfs/ -t ramfs
 wget -qO /root/tmpramfs/keyfile_encrypted https://decrypt.optimus-avocats.fr/$(</root/uid)_keyfile
+echo -e "\e[35mOUVERTURE DE LA PARTITION CRYPTEE...\e[0m"
 openssl rsautl -decrypt -inkey /root/private.pem -in /root/tmpramfs/keyfile_encrypted | /sbin/cryptsetup luksOpen /dev/sda2 cryptsda2
 umount /root/tmpramfs
 rmdir /root/tmpramfs
 
-#if [ -f /root/keyfile_encrypted ]
-#then
-  #echo -e "\e[35mOUVERTURE DE LA PARTITION CRYPTEE...\e[0m"
-  #openssl rsautl -decrypt -inkey /root/private.pem -in /root/keyfile_encrypted | /sbin/cryptsetup luksOpen /dev/sda2 cryptsda2
-#else
-  #echo -e "\e[35mOUVERTURE DE LA PARTITION CRYPTEE...\e[0m"
-  #/sbin/cryptsetup luksOpen /dev/sda2 cryptsda2
-#fi
+if [ -f /root/keyfile_encrypted ]
+then
+  echo -e "\e[35mOUVERTURE DE LA PARTITION CRYPTEE...\e[0m"
+  openssl rsautl -decrypt -inkey /root/private.pem -in /root/keyfile_encrypted | /sbin/cryptsetup luksOpen /dev/sda2 cryptsda2
+else
+  echo -e "\e[35mOUVERTURE DE LA PARTITION CRYPTEE...\e[0m"
+  /sbin/cryptsetup luksOpen /dev/sda2 cryptsda2
+fi
 
 mount /dev/mapper/cryptsda2 /srv
+
+sleep 0.5
+
+if [ -d /etc/mysql ]; then verbose systemctl restart mariadb; fi
