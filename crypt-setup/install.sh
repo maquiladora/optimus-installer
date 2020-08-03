58e286b1f6bb1ee0c7561a1f1c3920a7f36bd181
MetallianFR68 <lionel.vest@adaris.org>
2020-08-03 17:19:51 +0200

    *

--- crypt-setup/install.sh
+++ crypt-setup/install.sh
@@ -4,11 +4,14 @@ source /installer/config.sh
 
 if lsblk -o NAME -n /dev/sda2 2>/dev/null | grep -q 'sda2'
 then
+
   echo_green "==== CHIFFREMENT DU DISQUE ===="
+
   echo_magenta "Installation des paquets requis..."
   DEBIAN_FRONTEND=noninteractive apt-get -qq install keyboard-configuration &> /dev/null
   verbose apt-get -qq install cryptsetup cryptsetup-bin > /dev/null
   verbose apt-get -qq install curl
+
   echo_magenta "Création d'une clé de chiffrement..."
   mkdir /root/tmpramfs
   mount ramfs /root/tmpramfs/ -t ramfs
@@ -18,25 +21,32 @@ then
   openssl rsa -in /root/private.pem -outform PEM -pubout -out /root/tmpramfs/public.pem &> /dev/null
   openssl rsautl -encrypt -inkey /root/tmpramfs/public.pem -pubin -in /root/tmpramfs/keyfile -out /root/tmpramfs/keyfile_encrypted &> /dev/null
   sleep 0.5
+
   echo_magenta "Envoi de la clé de chiffrement sur le serveur distant"
   curl -X POST -F "$(</root/uid)=@/root/tmpramfs/keyfile_encrypted" https://decrypt.optimus-avocats.fr/index.php
+
   echo_magenta "Activation du chiffrement sur la partition"
   openssl rsautl -decrypt -inkey /root/private.pem -in /root/tmpramfs/keyfile_encrypted | /sbin/cryptsetup --batch-mode luksFormat /dev/sda2
   sleep 0.5
+
   echo_magenta "Ouverture de la partition chiffrée"
   UUID=$(</root/uid)
   openssl rsautl -decrypt -inkey /root/private.pem -in /root/tmpramfs/keyfile_encrypted | /sbin/cryptsetup luksOpen /dev/sda2 cryptsda2
   sleep 0.5
+
   echo_magenta "Formattage de la partition chiffrée au format EXT4"
   /sbin/mkfs.ext4 /dev/mapper/cryptsda2 &> /dev/null
   sleep 0.5
+
   echo_magenta "Sauvegarde du header"
   verbose cryptsetup luksHeaderBackup /dev/sda2 --header-backup-file /root/headerbackup
   sleep 0.5
+
   echo_magenta "Montage de la partition dans /srv"
   mount /dev/mapper/cryptsda2 /srv
-  umount /root/tmpramfs
-  rmdir /root/tmpramfs
+  #umount /root/tmpramfs
+  #rmdir /root/tmpramfs
+
 else
   echo_red "Opération impossible : la partition /dev/sda2 n'existe pas"
 fi
--- install.sh
+++ install.sh
@@ -5,11 +5,11 @@ then
   </dev/urandom tr -dc A-Z0-9 | head -c${1:-16} > /root/uid
 fi
 
-sudo apt-get --yes --force-yes update
-sudo apt-get --yes --force-yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
-sudo apt-get --yes --force-yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
-sudo apt-get --yes --force-yes remove cryptsetup-initramfs
-sudo apt-get --yes --force-yes install git
+DEBIAN_FRONTEND=noninteractive apt-get --yes --force-yes update
+DEBIAN_FRONTEND=noninteractive apt-get --yes --force-yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
+DEBIAN_FRONTEND=noninteractive apt-get --yes --force-yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
+DEBIAN_FRONTEND=noninteractive apt-get --yes --force-yes remove cryptsetup-initramfs
+DEBIAN_FRONTEND=noninteractive apt-get --yes --force-yes install git
 
 rm -R /installer
 mkdir /installer
