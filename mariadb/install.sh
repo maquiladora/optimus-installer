ed0bd108233f0254b806a290974e96bca94dfdbf
MetallianFR68 <lionel.vest@adaris.org>
2020-08-03 23:32:59 +0200

    *

--- mariadb/install.sh
+++ mariadb/install.sh
@@ -10,22 +10,17 @@ then
 
   echo_magenta "Installation du serveur MARIADB..."
 
-  debconf-set-selections <<< 'mariadb-server mysql-server/root_password password hopla'
-  debconf-set-selections <<< 'mariadb-server mysql-server/root_password_again password hopla'
   verbose apt-get -qq -y install mariadb-server
-
   verbose systemctl stop mariadb
-
   if [ ! -d "/srv/databases" ]
   then
     verbose mv /var/lib/mysql /srv
     verbose mv /srv/mysql /srv/databases
     verbose ln -s /srv/databases /var/lib/mysql
   fi
-
   sleep 0.5
-
   verbose systemctl start mariadb
+  verbose mariadb -u root -e "SET PASSWORD for 'root'@'localhost' = PASSWORD('test');FLUSH PRIVILEGES;"
 
   if [ ! $MARIADB_REMOTEACCESS ]; then echo_green "Voulez-vous autoriser la connexion à distance ?"; read -p "(o)ui / (n)on ? " -n 1 -e MARIADB_REMOTEACCESS; fi
   if [[ $MARIADB_REMOTEACCESS =~ ^[YyOo]$ ]]
@@ -33,7 +28,7 @@ then
     if [ $(which /sbin/ufw) ]; then verbose /sbin/ufw allow 3309; fi
     sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
     sed -i 's/3306/3309/g' /etc/mysql/mariadb.conf.d/50-server.cnf
-    verbose mariadb -u root -phopla -e "GRANT ALL ON *.* to 'root'@'%' IDENTIFIED BY 'test3' WITH GRANT OPTION;"
+    verbose mariadb -u root -phopla -e "GRANT ALL ON *.* to 'root'@'%' IDENTIFIED BY 'test' WITH GRANT OPTION;"
   fi
 
   echo_magenta "Le serveur MARIADB a été installé avec succès !"
