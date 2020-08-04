bc47befad22c325bc6822d82d3fb112497a9e2a2
MetallianFR68 <lionel.vest@adaris.org>
2020-08-04 18:48:14 +0200

    *

--- install.sh
+++ install.sh
@@ -2,23 +2,23 @@
 
 if [ ! -f /root/uid ]
 then
-  sudo </dev/urandom tr -dc A-Z0-9 | head -c${1:-16} > /root/uid
+  </dev/urandom tr -dc A-Z0-9 | head -c${1:-16} > /root/uid
 fi
 
-DEBIAN_FRONTEND=noninteractive sudo apt-get --yes --force-yes update
-DEBIAN_FRONTEND=noninteractive sudo apt-get --yes --force-yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
-DEBIAN_FRONTEND=noninteractive sudo apt-get --yes --force-yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
-DEBIAN_FRONTEND=noninteractive sudo apt-get --yes --force-yes remove cryptsetup-initramfs
-DEBIAN_FRONTEND=noninteractive sudo apt-get --yes --force-yes install git
+DEBIAN_FRONTEND=noninteractive apt-get --yes --force-yes update
+DEBIAN_FRONTEND=noninteractive apt-get --yes --force-yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
+DEBIAN_FRONTEND=noninteractive apt-get --yes --force-yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
+DEBIAN_FRONTEND=noninteractive apt-get --yes --force-yes remove cryptsetup-initramfs
+DEBIAN_FRONTEND=noninteractive apt-get --yes --force-yes install git
 
-sudo rm -R /installer
-sudo mkdir /installer
-sudo git clone -b vest https://github.com/MetallianFR68/optimus-installer /installer
+rm -R /installer
+mkdir /installer
+git clone -b vest https://github.com/MetallianFR68/optimus-installer /installer
 
-if ! sudo grep -q "source /installer/menu.sh" /root/.bashrc
+if ! grep -q "source /installer/menu.sh" /root/.bashrc
 then
   echo "source /installer/menu.sh" >> /root/.bashrc
 fi
 
-sudo chmod +x /installer/menu.sh
-sudo source /installer/menu.sh
+chmod +x /installer/menu.sh
+source /installer/menu.sh
