9dfab7c781261bfe89874f42896132f0529537b7
MetallianFR68 <lionel.vest@adaris.org>
2020-08-04 18:42:07 +0200

    *

--- install.sh
+++ install.sh
@@ -5,20 +5,20 @@ then
   </dev/urandom tr -dc A-Z0-9 | head -c${1:-16} > /root/uid
 fi
 
-DEBIAN_FRONTEND=noninteractive apt-get --yes --force-yes update
-DEBIAN_FRONTEND=noninteractive apt-get --yes --force-yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
-DEBIAN_FRONTEND=noninteractive apt-get --yes --force-yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
-DEBIAN_FRONTEND=noninteractive apt-get --yes --force-yes remove cryptsetup-initramfs
-DEBIAN_FRONTEND=noninteractive apt-get --yes --force-yes install git
+DEBIAN_FRONTEND=noninteractive sudo apt-get --yes --force-yes update
+DEBIAN_FRONTEND=noninteractive sudo apt-get --yes --force-yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
+DEBIAN_FRONTEND=noninteractive sudo apt-get --yes --force-yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
+DEBIAN_FRONTEND=noninteractive sudo apt-get --yes --force-yes remove cryptsetup-initramfs
+DEBIAN_FRONTEND=noninteractive sudo apt-get --yes --force-yes install git
 
-rm -R /installer
-mkdir /installer
-git clone -b vest https://github.com/MetallianFR68/optimus-installer /installer
+sudo rm -R /installer
+sudo mkdir /installer
+sudo git clone -b vest https://github.com/MetallianFR68/optimus-installer /installer
 
 if ! grep -q "source /installer/menu.sh" /root/.bashrc
 then
   echo "source /installer/menu.sh" >> /root/.bashrc
 fi
 
-chmod +x /installer/menu.sh
-source /installer/menu.sh
+sudo chmod +x /installer/menu.sh
+sudo source /installer/menu.sh
