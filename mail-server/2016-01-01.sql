f1af3bea9c8751b6d20c222a521b689f22c622cb
MetallianFR68 <lionel.vest@adaris.org>
2020-08-06 21:02:31 +0200

    *

--- mail-server/2016-01-01.sql
+++ mail-server/2016-01-01.sql
@@ -3,8 +3,8 @@ USE server;
 
 CREATE TABLE IF NOT EXISTS mailboxes (
   id int(11) NOT NULL AUTO_INCREMENT,
-  email varchar(255) NOT NULL UNIQUE,
-  password varchar(255) NOT NULL,
+  email varchar(128) NOT NULL UNIQUE,
+  password varbinary(128) NOT NULL,
   quota bigint(11) NOT NULL DEFAULT 0,
   status tinyint(1) NOT NULL DEFAULT 1,
   aliases text,
