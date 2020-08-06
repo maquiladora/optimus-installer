c66cfcf49752d675e6e6b334760c5863a2d6c1ea
MetallianFR68 <lionel.vest@adaris.org>
2020-08-06 07:26:15 +0200

    *

--- decrypt/install.sh
+++ decrypt/install.sh
@@ -2,27 +2,32 @@
 source /installer/functions.sh
 source /installer/config.sh
 
+echo
+echo_green "==== DECHIFFREMENT DE LA PARTITION CHIFREE ===="
+
 mkdir /root/tmpramfs
 mount ramfs /root/tmpramfs/ -t ramfs
 wget -qO /root/tmpramfs/keyfile_encrypted https://decrypt.optimus-avocats.fr/$(</root/uid)_keyfile
-echo -e "\e[35mOUVERTURE DE LA PARTITION CRYPTEE...\e[0m"
+echo_magenta "Ouverture de la partition cryptée via le serveur de clé distant..."
 openssl rsautl -decrypt -inkey /root/private.pem -in /root/tmpramfs/keyfile_encrypted | /sbin/cryptsetup luksOpen /dev/sda2 cryptsda2
 umount /root/tmpramfs
 rmdir /root/tmpramfs
 
 if [ -f /root/keyfile_encrypted ]
 then
-  echo -e "\e[35mOUVERTURE DE LA PARTITION CRYPTEE...\e[0m"
+  echo_magenta "Ouverture de la partition chiffrée avec une clé locale..."
   openssl rsautl -decrypt -inkey /root/private.pem -in /root/keyfile_encrypted | /sbin/cryptsetup luksOpen /dev/sda2 cryptsda2
 else
-  echo -e "\e[35mOUVERTURE DE LA PARTITION CRYPTEE...\e[0m"
+  echo_magenta "Ouverture de la partition chiffrée avec une clé saisie dans le terminal..."
   /sbin/cryptsetup luksOpen /dev/sda2 cryptsda2
 fi
 
+echo_magenta "Montage de la partition chiffrée..."
 mount /dev/mapper/cryptsda2 /srv
 
 sleep 0.5
 
+echo_magenta "Redémarrage des services"
 if [ -d /etc/mysql ]; then verbose systemctl restart mariadb; fi
 if [ -d /etc/mailboxes ]
 then
