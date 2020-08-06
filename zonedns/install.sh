343ef91a8c7dc283573ff2c6fdc0e1adbdec8e6e
MetallianFR68 <lionel.vest@adaris.org>
2020-08-07 00:06:37 +0200

    *

--- zonedns/install.sh
+++ zonedns/install.sh
@@ -1,14 +1,21 @@
-public_ip=$(  wget -qO- ipinfo.io/ip )
-local_ip=$( ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' )
+#!/bin/bash
+source /installer/functions.sh
+source /installer/config.sh
 
-echo_magenta "Voici les enregistrements DNS à renseigner dans votre nom de domaine $DOMAIN"
+echo_green "==== ZONE DNS ===="
+
+PUBLIC_IP=$(  wget -qO- ipinfo.io/ip )
+LOCAL_IP=$( ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' )
+
+echo_magenta "Voici les enregistrements DNS à renseigner dans votre nom de domaine $DOMAIN :"
 echo ""
 
-sed -e 's/$domain/'$domain'/g' -e 's/$public_ip/'$public_ip'/g' /srv/installer/dns/zone.conf
+sed -e 's/$domain/'$DOMAIN'/g' -e 's/$public_ip/'$PUBLIC_IP'/g' /srv/installer/dns/zone.conf
+cat /etc/dkim/keys/$DOMAIN/mail.txt
 echo ""
 
 
-echo_magenta "Dans votre routeur, ces ports doivent être redirigés vers le serveur dont l'adresse locale est : $local_ip : \e[0m"
+echo_magenta "Dans votre routeur, ces ports doivent être redirigés vers le serveur dont l'adresse locale est : $LOCAL_IP :"
 echo ""
 echo "22   SSH"
 echo "80   HTTP"
