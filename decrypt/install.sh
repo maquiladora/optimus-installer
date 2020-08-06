6d94cb0f2c5ef25314d4160a6e02316f28bd8560
MetallianFR68 <lionel.vest@adaris.org>
2020-08-06 07:37:22 +0200

    *

--- decrypt/install.sh
+++ decrypt/install.sh
@@ -3,23 +3,26 @@ source /installer/functions.sh
 source /installer/config.sh
 
 echo
-echo_green "==== DECHIFFREMENT DE LA PARTITION CHIFREE ===="
+echo_green "==== DECHIFFREMENT DE LA PARTITION CHIFFREE ===="
 
+echo_magenta "Ouverture de la partition cryptée via le serveur de clé distant..."
 mkdir /root/tmpramfs
 mount ramfs /root/tmpramfs/ -t ramfs
 wget -qO /root/tmpramfs/keyfile_encrypted https://decrypt.optimus-avocats.fr/$(</root/uid)_keyfile
-echo_magenta "Ouverture de la partition cryptée via le serveur de clé distant..."
 openssl rsautl -decrypt -inkey /root/private.pem -in /root/tmpramfs/keyfile_encrypted | /sbin/cryptsetup luksOpen /dev/sda2 cryptsda2
 umount /root/tmpramfs
 rmdir /root/tmpramfs
 
-if [ -f /root/keyfile_encrypted ]
+if lsblk -o NAME -n /dev/mapper/cryptsda2 2>/dev/null
 then
-  echo_magenta "Ouverture de la partition chiffrée avec une clé locale..."
-  openssl rsautl -decrypt -inkey /root/private.pem -in /root/keyfile_encrypted | /sbin/cryptsetup luksOpen /dev/sda2 cryptsda2
-else
-  echo_magenta "Ouverture de la partition chiffrée avec une clé saisie dans le terminal..."
-  /sbin/cryptsetup luksOpen /dev/sda2 cryptsda2
+  if [ -f /root/keyfile_encrypted ]
+  then
+    echo_magenta "Ouverture de la partition chiffrée avec une clé locale..."
+    openssl rsautl -decrypt -inkey /root/private.pem -in /root/keyfile_encrypted | /sbin/cryptsetup luksOpen /dev/sda2 cryptsda2
+  else
+    echo_magenta "Ouverture de la partition chiffrée avec une clé saisie dans le terminal..."
+    /sbin/cryptsetup luksOpen /dev/sda2 cryptsda2
+  fi
 fi
 
 echo_magenta "Montage de la partition chiffrée..."
@@ -28,8 +31,8 @@ mount /dev/mapper/cryptsda2 /srv
 sleep 0.5
 
 echo_magenta "Redémarrage des services"
-if [ -d /etc/mysql ]; then verbose systemctl restart mariadb; fi
-if [ -d /etc/mailboxes ]
+if [ -d /srv/databases ]; then verbose systemctl restart mariadb; fi
+if [ -d /srv/mailboxes ]
 then
   verbose systemctl restart postfix
   verbose systemctl restart devocot
