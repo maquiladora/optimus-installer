2eb7fc5e95f74f237c4a90ea162eae7b5100ecc9
MetallianFR68 <lionel.vest@adaris.org>
2020-08-07 13:56:17 +0200

    *

--- menu.sh
+++ menu.sh
@@ -127,6 +127,8 @@ case "$y" in
     rm -R /installer
     mkdir /installer
     git clone -b vest https://github.com/MetallianFR68/optimus-installer /installer
+    chmod +x /installer/menu.sh
+    source /installer/menu.sh
 		;;
 
   v)
