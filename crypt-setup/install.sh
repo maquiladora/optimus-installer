e7907f3fc03d13bebe29bd411e9055a0af9f74d1
MetallianFR68 <lionel.vest@adaris.org>
2020-08-07 18:35:42 +0200

    *

--- crypt-setup/install.sh
+++ crypt-setup/install.sh
@@ -13,26 +13,25 @@ then
   verbose apt-get -qq install curl
 
   echo_magenta "Création d'une clé de chiffrement..."
-  mkdir ~/tmpramfs
-  mount ramfs ~/tmpramfs/ -t ramfs
-  </dev/urandom tr -dc A-Za-z0-9 | head -c${1:-256} > ~/tmpramfs/keyfile
-  chmod 0400 ~/tmpramfs/keyfile
-  openssl genrsa -out ~/private.pem 4096 &> /dev/null
-  openssl rsa -in ~/private.pem -outform PEM -pubout -out ~/tmpramfs/public.pem &> /dev/null
-  openssl rsautl -encrypt -inkey ~/tmpramfs/public.pem -pubin -in ~/tmpramfs/keyfile -out ~/tmpramfs/keyfile_encrypted &> /dev/null
+  mkdir /root/tmpramfs
+  mount ramfs /root/tmpramfs/ -t ramfs
+  </dev/urandom tr -dc A-Za-z0-9 | head -c${1:-256} > /root/tmpramfs/keyfile
+  chmod 0400 /root/tmpramfs/keyfile
+  openssl genrsa -out /root/private.pem 4096 &> /dev/null
+  openssl rsa -in /root/private.pem -outform PEM -pubout -out /root/tmpramfs/public.pem &> /dev/null
+  openssl rsautl -encrypt -inkey /root/tmpramfs/public.pem -pubin -in /root/tmpramfs/keyfile -out /root/tmpramfs/keyfile_encrypted &> /dev/null
   sleep 0.5
 
   echo_magenta "Envoi de la clé de chiffrement sur le serveur distant"
-  curl -X POST -F "$(<~/uid)=@~/tmpramfs/keyfile_encrypted" https://decrypt.optimus-avocats.fr/index.php
-
+  curl -X POST -F "$(</root/uid)=@/root/tmpramfs/keyfile_encrypted" https://decrypt.optimus-avocats.fr/index.php
 
   echo_magenta "Activation du chiffrement sur la partition"
-  openssl rsautl -decrypt -inkey ~/private.pem -in ~/tmpramfs/keyfile_encrypted | /sbin/cryptsetup --batch-mode luksFormat /dev/sda2
+  openssl rsautl -decrypt -inkey /root/private.pem -in /root/tmpramfs/keyfile_encrypted | /sbin/cryptsetup --batch-mode luksFormat /dev/sda2
   sleep 0.5
 
   echo_magenta "Ouverture de la partition chiffrée"
-  UUID=$(<~/uid)
-  openssl rsautl -decrypt -inkey ~/private.pem -in ~/tmpramfs/keyfile_encrypted | /sbin/cryptsetup luksOpen /dev/sda2 cryptsda2
+  UUID=$(</root/uid)
+  openssl rsautl -decrypt -inkey /root/private.pem -in /root/tmpramfs/keyfile_encrypted | /sbin/cryptsetup luksOpen /dev/sda2 cryptsda2
   sleep 0.5
 
   echo_magenta "Formattage de la partition chiffrée au format EXT4"
@@ -40,13 +39,13 @@ then
   sleep 0.5
 
   echo_magenta "Sauvegarde du header"
-  verbose cryptsetup luksHeaderBackup /dev/sda2 --header-backup-file ~/headerbackup
+  verbose cryptsetup luksHeaderBackup /dev/sda2 --header-backup-file /root/headerbackup
   sleep 0.5
 
   echo_magenta "Montage de la partition dans /srv"
   mount /dev/mapper/cryptsda2 /srv
-  umount ~/tmpramfs
-  rmdir ~/tmpramfs
+  umount /root/tmpramfs
+  rmdir /root/tmpramfs
 
 else
   echo_red "Opération impossible : la partition /dev/sda2 n'existe pas"
