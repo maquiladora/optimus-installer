2ed75559958b4ba64ac107fb032ccc539197516f
MetallianFR68 <lionel.vest@adaris.org>
2020-08-08 22:05:01 +0200

    *

--- mariadb/2020-08-01.sql
+++ mariadb/2020-08-01.sql
@@ -1,7 +1,7 @@
-CREATE DATABASE IF NOT EXISTS `users` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+CREATE DATABASE IF NOT EXISTS users CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `users`;
 
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE IF NOT EXISTS users (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `status` tinyint(1) unsigned NOT NULL DEFAULT '0',
   `email` varchar(128) NOT NULL UNIQUE,
