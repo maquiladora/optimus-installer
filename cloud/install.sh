fcb65901ec9fd0016c7ae172fe647854e96e2d0a
MetallianFR68 <lionel.vest@adaris.org>
2020-08-08 14:40:53 +0200

    *

--- cloud/install.sh
+++ cloud/install.sh
@@ -24,9 +24,10 @@ then
     echo_magenta "L'espace d'hébergement cloud.$DOMAIN a été installé avec succès !"
 
   php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-  php -r "if (hash_file('sha384', 'composer-setup.php') === 'e5325b19b381bfd88ce90a5ddb7823406b2a38cff6bb704b0acc289a09c8128d4a8ce2bbafcd1fcbdc38666422fe2806') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+  php -r "if (hash_file('sha384', 'composer-setup.php') != 'e5325b19b381bfd88ce90a5ddb7823406b2a38cff6bb704b0acc289a09c8128d4a8ce2bbafcd1fcbdc38666422fe2806') unlink('composer-setup.php'); echo PHP_EOL;"
   php composer-setup.php --install-dir /srv/cloud
   php -r "unlink('composer-setup.php');"
+  verbose /srv/cloud/composer require sabre/dav ~3.2.0
 
 
   verbose systemctl restart apache2
