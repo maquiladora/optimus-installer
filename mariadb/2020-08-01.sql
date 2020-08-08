00edf55e5f14238ee5db4e2a95997f801ac58e68
MetallianFR68 <lionel.vest@adaris.org>
2020-08-08 22:06:16 +0200

    *

--- mariadb/2020-08-01.sql
+++ mariadb/2020-08-01.sql
@@ -1,7 +1,7 @@
-CREATE DATABASE IF NOT EXISTS users CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+CREATE DATABASE IF NOT EXISTS `users` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `users`;
 
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `status` tinyint(1) unsigned NOT NULL DEFAULT '0',
   `email` varchar(128) NOT NULL UNIQUE,
@@ -9,6 +9,6 @@ CREATE TABLE IF NOT EXISTS users (
   `created` datetime,
   `updated` datetime,
   `token` varchar(32) UNIQUE,
-  `token_expiration` datetime
+  `token_expiration` datetime,
   PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4;
