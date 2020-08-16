1fbb8e2587d8902635292b1fa896978cf5d58b17
Lionel VEST <lionel.vest@adaris.org>
2020-08-16 10:15:27 +0200

    *

--- backup/install.sh
+++ backup/install.sh
@@ -5,32 +5,17 @@ source /installer/config.sh
 verbose apt-get install rsync sendmail
 
 echo
-echo_green "==== SAUVEGARDE AUTOMATIQUE DES BASES DE DONNES ===="
-if [ ! $BACKUP_DBBACKUP_AREYOUSURE ]; then echo_green "Souhaitez vous sauvegarder automatiquement les bases de données chaque jour à 23h30 ?"; read -p "(o)ui / (n)on ? " -n 1 -e BACKUP_DBBACKUP_AREYOUSURE; fi
-if [[ $BACKUP_DBBACKUP_AREYOUSURE =~ ^[YyOo]$ ]]
+echo_green "==== SAUVEGARDE AUTOMATIQUE DES BASES DE DONNEES ===="
+if [ ! $BACKUP_AREYOUSURE ]; then echo_green "Souhaitez vous mettre en place les sauvegardes incrémentielles ?"; read -p "(o)ui / (n)on ? " -n 1 -e BACKUP_AREYOUSURE; fi
+if [[ $BACKUP_AREYOUSURE =~ ^[YyOo]$ ]]
 then
-  verbose apt-get install php php-mysql php-zip
-  verbose mkdir -p /srv/db-backup
-  envsubst '${DOMAIN}' < /installer/backup/db-backup.php > /srv/db-backup.php
-  cp /installer/backup/db-backup.timer /etc/systemd/system/db-backup.timer
-  cp /installer/backup/db-backup.service /etc/systemd/system/db-backup.service
-  systemctl enable db-backup.timer
-  systemctl start db-backup.timer
-fi
-
-echo
-echo_green "==== SAUVEGARDE INCREMENTIELLE A DISTANCE ===="
-if [ ! $BACKUP_INCREMENTS_AREYOUSURE ]; then echo_green "Souhaitez vous effectuer une sauvegarde incrémentielle chaque jour à 2h30 ?"; read -p "(o)ui / (n)on ? " -n 1 -e BACKUP_INCREMENTS_AREYOUSURE; fi
-if [[ $BACKUP_INCREMENTS_AREYOUSURE =~ ^[YyOo]$ ]]
-then
-  verbose apt-get install rsync
-  envsubst '${DOMAIN}' < /installer/backup/increments-backup.sh > /srv/increments-backup.sh
-  cp /installer/backup/increments-backup.timer /etc/systemd/system/increments-backup.timer
-  cp /installer/backup/increments-backup.service /etc/systemd/system/increments-backup.service
-  systemctl enable increments-backup.timer
-  systemctl start increments-backup.timer
-
+  verbose apt-get install rsync tar
   ssh-keygen -y -f /root/private.pem | ssh debian@192.168.0.9 "mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys"
-
-
+  verbose mkdir -p /srv/db-backup
+  verbose mkdir -p /srv/increments
+  envsubst '${DOMAIN}' < /installer/backup/allspark-backup.sh > /srv/allspark-backup.php
+  cp /installer/backup/allspark-backup.timer /etc/systemd/system/allspark-backup.timer
+  cp /installer/backup/allspark-backup.service /etc/systemd/system/allspark-backup.service
+  systemctl enable allspark-backup.timer
+  systemctl start allspark-backup.timer
 fi
