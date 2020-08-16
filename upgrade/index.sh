f75f9d2e61547483468182e4ff5c3b67e0c53752
Lionel VEST <lionel.vest@adaris.org>
2020-08-17 01:27:50 +0200

    *

--- upgrade/index.sh
+++ upgrade/index.sh
@@ -5,4 +5,4 @@ DEBIAN_FRONTEND=noninteractive sudo apt-get --yes update
 DEBIAN_FRONTEND=noninteractive sudo apt-get --yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
 DEBIAN_FRONTEND=noninteractive sudo apt-get --yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
 
-update_conf LAST_UPGRADE date +'%Y%m%d'
+update_conf LAST_UPGRADE $(date +'%Y%m%d')
