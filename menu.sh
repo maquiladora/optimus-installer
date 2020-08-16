bac0e8e72c9ea5bc2123b32c34e2ce1bfef56c48
Lionel VEST <lionel.vest@adaris.org>
2020-08-16 21:03:29 +0200

    *

--- menu.sh
+++ menu.sh
@@ -38,7 +38,7 @@ tput cup 17 3; if [ -d "/srv/api" ]; then echo_green "m. Installer l'api de comu
 tput cup 18 3; if [ -d "/srv/optimus" ]; then echo_green "n. Installer le client OPTIMUS-AVOCATS (facultatif)"; else echo_red "n. Installer le client OPTIMUS-AVOCATS (facultatif)"; fi
 tput cup 19 3; echo_green "o. Configuration de la zone DNS"
 tput cup 20 3; if [ -d "/etc/letsencrypt" ]; then echo_green "q. Installer les certificats SSL"; else echo_red "q. Installer les certificats SSL"; fi
-tput cup 21 3; if [ -d "/etc/rsync" ]; then echo_green "r. Installer les scripts de sauvegardes"; else echo_red "r. Installer RSYNC"; fi
+tput cup 21 3; if [ -d "/etc/rsync" ]; then echo_green "r. Installer les scripts de sauvegardes"; else echo_red "r. Installer les scripts de sauvegardes"; fi
 
 tput cup 23 3; echo_green "u. Update Installer"
 tput cup 24 3; echo_green "v. Reboot server"
