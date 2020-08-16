f01d002e171c29e887b618f48e44100202fd3f0f
Lionel VEST <lionel.vest@adaris.org>
2020-08-16 22:55:46 +0200

    *

--- menu.sh
+++ menu.sh
@@ -3,13 +3,6 @@ source /installer/functions.sh
 
 require PART_TO_ENCRYPT
 
-if [ $PART_TO_ENCRYPT ]
-then
-  echo "oui"
-else
-  echo "non"
-fi
-
 while : ; do
 
 clear
@@ -38,7 +31,7 @@ tput cup 21 3; if [ -d "/etc/rsync" ]; then echo_green "r. Installer les scripts
 tput cup 23 3; echo_green "u. Update Installer"
 tput cup 24 3; echo_green "v. Reboot server"
 tput cup 25 3; echo_green "x. Quit"
-tput cup 26 3; echo_green "y. ${1}"
+tput cup 26 3; if [ $PART_TO_ENCRYPT ]; then echo "oui"; else echo "non"; fi
 tput cup 27 3; echo_green "z. $SECURE_CHANGEROOTPASS"
 
 #tput cup 24 3; echo -ne "\e[32m s. Save \e[0m"
