a7d38ba4f82f4b6d499179e0c1a7a0e68a7f6355
Lionel VEST <lionel.vest@adaris.org>
2020-08-19 00:38:39 +0200

    *

--- mariadb/install.sh
+++ mariadb/install.sh
@@ -48,19 +48,6 @@ echo_green "==== CONNEXION A DISTANCE A LA BASE DE DONNEES ===="
 if [ ! $MARIADB_REMOTEACCESS ]; then echo_green "Voulez-vous autoriser la connexion à distance ?"; read -p "(o)ui / (n)on ? " -n 1 -e MARIADB_REMOTEACCESS; fi
 if [[ $MARIADB_REMOTEACCESS =~ ^[YyOo]$ ]]
 then
-  if [ ! $MARIADB_REMOTE_ROOT_PASSWORD ]
-  then
-    echo_green "Voulez vous générer un mot de passe automatiquement ?"
-    read -n 1 -p "(o)ui / (n)on ? " -e MARIADB_REMOTE_ROOT_PASSWORD_GENERATE
-    if [[$MARIADB_REMOTE_ROOT_PASSWORD_GENERATE =~ ^[YyOo]$ ]]
-    then
-      MARIADB_REMOTE_ROOT_PASSWORD=$(</dev/urandom tr -dc A-Za-z0-9 | head -c${1:-32})
-    else
-      echo_magenta "Veuillez renseigner le mot de passe de l'utilisateur ROOT distant : "
-      read MARIADB_REMOTE_ROOT_PASSWORD;
-    fi
-  fi
-
   if [ $(which /sbin/ufw) ]; then verbose /sbin/ufw allow 3306; fi
   sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
   verbose mariadb -u root -e "GRANT ALL ON *.* to 'root'@'%' IDENTIFIED BY '$MARIADB_REMOTE_ROOT_PASSWORD' WITH GRANT OPTION;"
