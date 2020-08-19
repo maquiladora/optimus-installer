64dae5304e8635b93c7f852a3dfa32aa0543e55e
Lionel VEST <lionel.vest@adaris.org>
2020-08-19 22:49:51 +0200

    *

--- upgrade/install.sh
+++ upgrade/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 source /etc/allspark/functions.sh
 
-DEBIAN_FRONTEND=noninteractive sudo apt --yes update
-DEBIAN_FRONTEND=noninteractive sudo apt --yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
-DEBIAN_FRONTEND=noninteractive sudo apt --yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
+DEBIAN_FRONTEND=noninteractive sudo apt -qq --yes update
+DEBIAN_FRONTEND=noninteractive sudo apt -qq --yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
+DEBIAN_FRONTEND=noninteractive sudo apt -qq --yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
 
 update_conf LAST_UPGRADE $(date +'%Y%m%d')
