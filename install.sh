3b51baa6dc5d38f881ccd20263a3ebab25fac0a3
Lionel VEST <lionel.vest@adaris.org>
2020-08-21 00:26:26 +0200

    *

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-DEBIAN_FRONTEND=noninteractive sudo apt -qq --yes update
-DEBIAN_FRONTEND=noninteractive sudo apt -qq --yes remove cryptsetup-initramfs
-DEBIAN_FRONTEND=noninteractive sudo apt -qq --yes install git unzip zip sudo
+DEBIAN_FRONTEND=noninteractive sudo apt-get -qq --yes update
+DEBIAN_FRONTEND=noninteractive sudo apt-get-qq --yes remove cryptsetup-initramfs
+DEBIAN_FRONTEND=noninteractive sudo apt-get -qq --yes install git unzip zip sudo
 
 sudo rm -R /etc/allspark
 sudo mkdir /etc/allspark
