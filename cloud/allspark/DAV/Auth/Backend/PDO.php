2c68b20d5f9ee75a140fdd9f5bf6a826c26af0e6
Lionel VEST <lionel.vest@adaris.org>
2020-08-22 06:14:14 +0200

    *

--- cloud/allspark/DAV/Auth/Backend/PDO.php
+++ cloud/allspark/DAV/Auth/Backend/PDO.php
@@ -14,7 +14,7 @@ class PDO extends \Sabre\DAV\Auth\Backend\AbstractDigest
 
 	function getDigestHash($realm, $username)
 	{
-		$stmt = $this->pdo->prepare('SELECT MD5(CONCAT(email,":","' . $realm . '",":",AES_DECRYPT(password,'$AES_KEY'))) FROM ' . $this->tableName . ' WHERE email = ?');
+		$stmt = $this->pdo->prepare('SELECT MD5(CONCAT(email,":","' . $realm . '",":",AES_DECRYPT(password,"$AES_KEY"))) FROM ' . $this->tableName . ' WHERE email = ?');
 		$stmt->execute([$username]);
 		return $stmt->fetchColumn() ?: null;
 	}
