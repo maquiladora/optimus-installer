e50a91de7c025bfb36ecefd379d4aa7c0f8d83d2
Lionel VEST <lionel.vest@adaris.org>
2020-08-22 06:27:01 +0200

    *

--- upgrade/install.sh
+++ upgrade/install.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 source /etc/allspark/functions.sh
 
-echo_magenta "Mise à jour du système (UPDATE + UPGRADE + DIST UPGRADE)"
+echo_green "==== MISE A JOUR DU SYSTEME ===="
 
+echo_magenta "Update"
 DEBIAN_FRONTEND=noninteractive sudo apt-get -qq -y update
+
+echo_magenta "Upgrade"
 DEBIAN_FRONTEND=noninteractive sudo apt-get -qq -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
+
+echo_magenta "Dist-Upgrade"
 DEBIAN_FRONTEND=noninteractive sudo apt-get -qq -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
 
 update_conf LAST_UPGRADE $(date +'%Y%m%d')
