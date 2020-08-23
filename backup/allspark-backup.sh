4535e539aa14541e23856616750e7c4978278371
Lionel VEST <lionel.vest@adaris.org>
2020-08-24 01:18:32 +0200

    *

--- backup/allspark-backup.sh
+++ backup/allspark-backup.sh
@@ -1,35 +1,29 @@
 #!/bin/sh
-clear
 
-# mysqldump --all-databases --ignore-database roundcube | gzip > /srv/db-backup/`date +%Y-%m-%d.sql.gz`
+echo "From: prime@$DOMAIN" > /srv/email.txt
+echo "To: prime@$DOMAIN" >> /srv/email.txt
+echo "Subject: BACKUP REPORT" >> /srv/email.txt
 
-mysql -N -e 'show databases' | while read dbname; do mysqldump --complete-insert --routines --triggers --single-transaction "$dbname" > "/srv/db-backup/$dbname".sql; done
-zip /srv/db-backup/`date +%Y-%m-%d.zip` /srv/db-backup/*.sql
+echo "DATABASE BACKUP" >> /srv/email.txt
+mysql -N -e 'show databases' | while read dbname; do if [ $dbname != 'information_schema' ] && [ $dbname != 'performance_schema' ]; then mysqldump --routines --triggers --single-transaction "$dbname" > "/srv/db-backup/$dbname".sql; fi done
+cd /srv/db-backup/
+zip `date +%Y-%m-%d.zip` *.sql >> /srv/email.txt
+
+/usr/sbin/sendmail -t < /srv/email.txt
+
 rm /srv/db-backup/*.sql
-
-exit 0
+rm /srv/email.txt
 
 # rsync --recursive --delete --copy-links --perms --owner --group --times --compress --verbose --progress --stats --backup --backup-dir=debian@$BACKUP_SERVER:/srv/increments/`date +%Y-%m-%d--%Hh%M` --exclude '/srv/increments' -e "ssh -p 22 -i /root/private.pem" '/srv/' 'debian@$BACKUP_SERVER:/srv/' | tee rsync.log
 
-if [ $? -eq 0 ]; then echo "Subject: CONQUEST : BACKUP SUCCESSFULL" > email.txt; else echo "Subject: CONQUEST : BACKUP ERROR" > email.txt; fi;
 
-exit 0;
+#ssh -i /root/private.pem debian@$BACKUP_SERVER <<'ENDSSH'
+#cd /srv/increments
+#for dir in */
+#do
+#  base=$(basename "$dir")
+#  tar -czf "${base}.tar.gz" "$dir" --remove-files
+#done
+#ENDSSH
 
-ssh -i /root/private.pem debian@$BACKUP_SERVER <<'ENDSSH'
-cd /srv/increments
-for dir in */
-do
-  base=$(basename "$dir")
-  tar -czf "${base}.tar.gz" "$dir" --remove-files
-done
-ENDSSH
-
-rsync --recursive --delete --copy-links --perms --owner --group --times --compress --verbose --progress --stats -e "ssh -p 7822 -i /root/private.pem" 'debian@$DOMAIN:/srv/increments' '/srv/increments' | tee rsync.log
-
-echo "From: prime@$DOMAIN" >> email.txt;
-echo "To: prime@$DOMAIN" >> email.txt;
-cat rsync.log >> email.txt;
-/usr/sbin/sendmail -t < email.txt;
-
-rm email.txt;
-rm rsync.log;
+#rsync --recursive --delete --copy-links --perms --owner --group --times --compress --verbose --progress --stats -e "ssh -p 7822 -i /root/private.pem" 'debian@$DOMAIN:/srv/increments' '/srv/increments' | tee rsync.log
