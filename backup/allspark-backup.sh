1457785926d1c0d48f5d3a40c82f5c15db57b14a
Lionel VEST <lionel.vest@adaris.org>
2020-08-24 00:13:07 +0200

    *

--- backup/allspark-backup.sh
+++ backup/allspark-backup.sh
@@ -3,7 +3,7 @@ clear
 
 mysqldump --all-databases --ignore-database roundcube | gzip > /srv/db-backup/`date +%Y-%m-%d.sql.gz`
 
-rsync --recursive --delete --copy-links --perms --owner --group --times --compress --verbose --progress --stats --backup --backup-dir=debian@$BACKUPSERVER:/srv/increments/`date +%Y-%m-%d--%Hh%M` --exclude '/srv/increments' -e "ssh -p 22 -i /root/private.pem" '/srv/' 'debian@$BACKUPSERVER:/srv/' | tee rsync.log
+rsync --recursive --delete --copy-links --perms --owner --group --times --compress --verbose --progress --stats --backup --backup-dir=debian@$BACKUP_SERVER:/srv/increments/`date +%Y-%m-%d--%Hh%M` --exclude '/srv/increments' -e "ssh -p 22 -i /root/private.pem" '/srv/' 'debian@$BACKUPSERVER:/srv/' | tee rsync.log
 
 if [ $? -eq 0 ]; then echo "Subject: CONQUEST : BACKUP SUCCESSFULL" > email.txt; else echo "Subject: CONQUEST : BACKUP ERROR" > email.txt; fi;
 
