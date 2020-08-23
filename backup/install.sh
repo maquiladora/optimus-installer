c067522694d70b4158605830256b9bd129056f16
Lionel VEST <lionel.vest@adaris.org>
2020-08-24 00:10:12 +0200

    *

--- backup/install.sh
+++ backup/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source /etc/allspark/functions.sh
+if [ -z $DOMAIN ]; then require DOMAIN string "Veuillez renseigner votre nom de domaine :"; source /root/.allspark; fi
 if [ -z $MODULE_BACKUP ]; then require MODULE_BACKUP yesno "Voulez-vous installer le module de sauvegarde ?"; source /root/.allspark; fi
 if [ -z $BACKUP_SERVER ]; then require BACKUP_SERVER string "Veuillez renseigner l'adresse IP du serveur de sauvegarde"; source /root/.allspark; fi
 source /root/.allspark
@@ -16,7 +17,8 @@ then
   ssh-keygen -y -f /root/private.pem | ssh debian@$BACKUP_SERVER "mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys"
   verbose mkdir -p /srv/db-backup
   verbose mkdir -p /srv/increments
-  envsubst '${DOMAIN}' < /etc/allspark/backup/allspark-backup.sh > /srv/allspark-backup.sh
+  envsubst '${DOMAIN} ${BACKUP_SERVER}' < /etc/allspark/backup/allspark-backup.sh > /srv/allspark-backup.sh
+  chmod 744 /srv/allspark-backup.sh
   cp /etc/allspark/backup/allspark-backup.timer /etc/systemd/system/allspark-backup.timer
   cp /etc/allspark/backup/allspark-backup.service /etc/systemd/system/allspark-backup.service
   systemctl enable allspark-backup.timer
