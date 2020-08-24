30086395d57d06c8de75c63b333b888c20184cc4
Lionel VEST <lionel.vest@adaris.org>
2020-08-24 02:52:25 +0200

    *

--- backup/allspark-backup.sh
+++ backup/allspark-backup.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-echo "From: prime@$DOMAIN" > /srv/email.txt
-echo "To: prime@$DOMAIN" >> /srv/email.txt
-echo "Subject: BACKUP REPORT" >> /srv/email.txt
+echo "From: prime@$DOMAIN" > /var/log/allspark-backup.log
+echo "To: prime@$DOMAIN" >> /var/log/allspark-backup.log
+echo "Subject: BACKUP REPORT" >> /var/log/allspark-backup.log
 
-echo "DATABASE BACKUP" >> /srv/email.txt
+echo "DATABASE BACKUP" >> /var/log/allspark-backup.log
 mysql -N -e 'show databases' | while read dbname; do if [ $dbname != 'information_schema' ] && [ $dbname != 'performance_schema' ]; then mysqldump --routines --triggers --single-transaction "$dbname" > "/srv/db-backup/$dbname".sql; fi done
 cd /srv/db-backup/
-zip `date +%Y-%m-%d.zip` *.sql >> /srv/email.txt
+zip `date +%Y-%m-%d.zip` *.sql >> /var/log/allspark-backup.log
+rm /srv/db-backup/*.sql
 
 echo
-echo "SERVER BACKUP" >> /srv/email.txt
-rdiff-backup -v 7 --remote-schema "ssh -i /root/private.pem %s rdiff-backup --server" /srv debian@$BACKUP_SERVER::/srv >> /srv/email.txt
+echo "SERVER BACKUP" >> /var/log/allspark-backup.log
+rdiff-backup -v 7 --remote-schema "ssh -i /root/private.pem %s rdiff-backup --server" /srv debian@$BACKUP_SERVER::/srv >> /var/log/allspark-backup.log
 
-/usr/sbin/sendmail -t < /srv/email.txt
+/usr/sbin/sendmail -t < /var/log/allspark-backup.log
 
-rm /srv/db-backup/*.sql
-rm /srv/email.txt
+rm /var/log/allspark-backup.log
