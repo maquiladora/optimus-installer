93a23b4a2dab75448c37f498eae90b1f0bde4991
Lionel VEST <lionel.vest@adaris.org>
2020-09-06 00:59:22 +0200

    *

--- api/install/api_allspark/login.php
+++ api/install/api_allspark/login.php
@@ -9,10 +9,12 @@ include_once 'config.php';
 include_once 'connect.php';
 include_once 'api_allspark/user.php';
 
-
+$database = new Database();
+$db = $database->getConnection();
+$user = new User($db);
 $data = json_decode(file_get_contents("php://input"));
 
-echo $user->email;
+echo $data->password;
 http_response_code(200);
 
 $user->email = $data->email;
