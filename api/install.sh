1c7a1e7212527f3d5299cc669e0a51b0b63a8ed5
Lionel VEST <lionel.vest@adaris.org>
2020-09-05 07:12:14 +0200

    *

--- api/install.sh
+++ api/install.sh
@@ -21,7 +21,6 @@ then
   cd /srv/api
   cp -R /etc/allspark/api/install/*.* /srv/api/
   envsubst '${MARIADB_ADMIN_USER} ${MARIADB_ADMIN_PASSWORD} ${DOMAIN} ${AES_KEY}' < /etc/allspark/api/install/connect.php > /srv/api/connect.php
-  envsubst '${MARIADB_ADMIN_USER} ${MARIADB_ADMIN_PASSWORD} ${DOMAIN} ${AES_KEY}' < /etc/allspark/api/install/core.php > /srv/api/core.php
   envsubst '${MARIADB_ADMIN_USER} ${MARIADB_ADMIN_PASSWORD} ${DOMAIN} ${AES_KEY}' < /etc/allspark/api/install/login.php > /srv/api/login.php
 
   echo_magenta "Redémarrage des services"
