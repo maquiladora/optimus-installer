ce06c2c24c2b824af7700b57e12806a289cfa086
Lionel VEST <lionel.vest@adaris.org>
2020-09-05 09:44:20 +0200

    *

--- api/install/index.php
+++ api/install/index.php
@@ -2,7 +2,7 @@
 $url = parse_url($_SERVER['REQUEST_URI']);
 $path = explode('/',$url['path']);
 
-$modules = scandir();
+$modules = scandir('.');
 foreach ($modules as $module)
   if (substr($module,0,4) == 'api_')
     include_once $module . '/index.php';
