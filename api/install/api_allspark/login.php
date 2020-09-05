1681a18c66b9561201df1a082fb1e841eb39b79f
Lionel VEST <lionel.vest@adaris.org>
2020-09-05 23:57:22 +0200

    *

--- api/install/api_allspark/login.php
+++ api/install/api_allspark/login.php
@@ -5,6 +5,7 @@ header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, Accept, Origin");
 http_response_code(200);
+echo $_POST['email'] . ' / ' . echo $_POST['password'];
 exit;
 include_once 'config.php';
 include_once 'connect.php';
