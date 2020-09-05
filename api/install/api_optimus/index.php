313def3149b321b9a30060b98cfe01f6156b6d50
Lionel VEST <lionel.vest@adaris.org>
2020-09-05 09:53:14 +0200

    *

--- api/install/api_optimus/index.php
+++ api/install/api_optimus/index.php
@@ -1,7 +1,6 @@
 <?php
 if ($path[1] == 'contacts')
 {
-  print_r($path);
   $contact['id'] = $path[2];
   include_once 'contact.php';
 }
--- api/install/index.php
+++ api/install/index.php
@@ -4,5 +4,6 @@ $path = explode('/',$url['path']);
 
 $modules = scandir('.');
 foreach ($modules as $module)
-    echo $module . '/index.php';
+  if (substr($module,0,4) == 'api_')
+    include_once $module . '/index.php';
 ?>
