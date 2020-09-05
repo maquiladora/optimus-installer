4e4bfa6add7d0512b53d0c64f9a371c02090e5b4
Lionel VEST <lionel.vest@adaris.org>
2020-09-05 08:18:41 +0200

    *

--- api/install/auth.php
+++ api/install/auth.php
@@ -26,11 +26,13 @@ if($token)
   {
       http_response_code(401);
       echo json_encode(array("message" => "Access denied", "error" => $e->getMessage()));
+      exit;
   }
 }
 else
 {
   http_response_code(401);
   echo json_encode(array("message" => "Access denied"));
+  exit;
 }
 ?>
