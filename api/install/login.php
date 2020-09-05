7a35542123d3f5650e524d329be91d54319eaa84
Lionel VEST <lionel.vest@adaris.org>
2020-09-05 06:10:19 +0200

    *

--- api/install/login.php
+++ api/install/login.php
@@ -32,7 +32,7 @@ include_once 'libs/php-jwt/src/JWT.php';
 use \Firebase\JWT\JWT;
 
 // check if email exists and if password is correct
-if($email_exists && password_verify(openssl_encrypt('W26b3RTE8mj4L3Su6GJBjz0qXtPIcNaM', 'aes-128-ecb', '$AES_KEY'),$user->password))
+if($email_exists)
 {
     $token = array(
        "iss" => $iss,
