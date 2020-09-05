8aa7cf4bc6a01202d38573dba7252a31fadb95d9
Lionel VEST <lionel.vest@adaris.org>
2020-09-05 23:42:12 +0200

    *

--- api/install/api_allspark/login.php
+++ api/install/api_allspark/login.php
@@ -4,6 +4,7 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+ header("Access-Control-Allow-Origin: *");
 
 include_once 'config.php';
 include_once 'connect.php';
