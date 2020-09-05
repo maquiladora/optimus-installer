c61dfa80465a5d42cd3203710e261df336bff580
Lionel VEST <lionel.vest@adaris.org>
2020-09-06 01:22:56 +0200

    *

--- www/index.php
+++ www/index.php
@@ -13,6 +13,8 @@
   {
     var xhr = new XMLHttpRequest();
     xhr.open("POST", 'https://api.$DOMAIN/allspark/login', true);
+    xhr.setRequestHeader('Accept', 'application/json, text/plain, */*"');
+    xhr.setRequestHeader('Content-Type', 'application/json;charset=utf-8');
     xhr.onreadystatechange = function()
     {
       if (this.readyState === XMLHttpRequest.DONE && this.status === 200)
