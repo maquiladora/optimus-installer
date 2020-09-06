34b6294f3deb0f527b7f4f3171e4aee100ee2d4d
Lionel VEST <lionel.vest@adaris.org>
2020-09-06 23:27:27 +0200

    *

--- api/install/api_allspark/status.php
+++ api/install/api_allspark/status.php
@@ -11,6 +11,19 @@ $token = substr(getallheaders()['Authorization'],7);
 include_once 'auth.php';
 
 //APACHE
-exec('systemctl status apache2', $apache_status);
-print_r($apache_status);
+
+function is_active($app)
+{
+  exec('systemctl status ' . $app, $output);
+  foreach ($output as $line)
+    if (strpos($line, 'active (running)') !== false)
+      return 'active';
+  return 'inactive';
+}
+
+$status['apache']['status'] = is_active('apache2');
+$status['mariadb']['status'] = is_active('mariadb');
+$status['postfix']['status'] = is_active('postfix');
+
+echo json_encode($status);
 ?>
