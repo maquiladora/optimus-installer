9b05e854dd96c5297bc70611de74f7ada90e9078
Lionel VEST <lionel.vest@adaris.org>
2020-09-07 00:35:24 +0200

    *

--- api/install/api_allspark/status.php
+++ api/install/api_allspark/status.php
@@ -23,13 +23,25 @@ function get_status($app)
 
 function get_version($app)
 {
-  return system("dpkg -s " . $app . " | grep '^Version:' | cut -c 10- | cut -f1 -d'-' | cut -f1 -d'+'");
+  return system("dpkg -s " . $app . " | grep '^Version:' | cut -c 10- | cut -f1 -d'-' | cut -f1 -d'+' | cut -f2 -d':'");
 }
 
 $status['apache']['status'] = get_status('apache2');
 $status['apache']['version'] = get_version('apache2');
-$status['mariadb']['status'] = get_status('mariadb');
-$status['postfix']['status'] = get_status('postfix');
+$status['apache']['status'] = get_status('php');
+$status['apache']['version'] = get_version('php');
+$status['apache']['status'] = get_status('mariadb');
+$status['apache']['version'] = get_version('mariadb');
+$status['apache']['status'] = get_status('postfix');
+$status['apache']['version'] = get_version('postfix');
+$status['apache']['status'] = get_status('dovecot-core');
+$status['apache']['version'] = get_version('dovecot-core');
+$status['apache']['status'] = get_status('spamassassin');
+$status['apache']['version'] = get_version('spamassassin');
+$status['apache']['status'] = get_status('clamav');
+$status['apache']['version'] = get_version('clamav');
+$status['apache']['status'] = get_status('rdiff-backup');
+$status['apache']['version'] = get_version('rdiff-backup');
 
 echo json_encode(array("message" => "", "response" => $status));
 ?>
