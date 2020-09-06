93b5648f61a251aada95a215f2ebf133d20434cb
Lionel VEST <lionel.vest@adaris.org>
2020-09-06 02:30:07 +0200

    *

--- api/install/api_allspark/login.php
+++ api/install/api_allspark/login.php
@@ -3,7 +3,7 @@ header("Access-Control-Allow-Origin: https://demoptimus.fr");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, Accept, Origin");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Access-Control-Allow-Origin, Authorization, X-Requested-With, Accept, Origin");
 
 include_once 'config.php';
 include_once 'connect.php';
