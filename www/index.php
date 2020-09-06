fb7a358c7269235c3ce5d1e6ad3b9cc94b348a47
Lionel VEST <lionel.vest@adaris.org>
2020-09-06 02:10:01 +0200

    *

--- www/index.php
+++ www/index.php
@@ -1,3 +1,10 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, Accept, Origin");
+ ?>
 <html>
   <body>
     <label for="email">Email :</label><br><input id="email" name="email"/>
