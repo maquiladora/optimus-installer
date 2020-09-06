8a22a53600ad815412f8e754dc432e31eb65ce43
Lionel VEST <lionel.vest@adaris.org>
2020-09-07 01:04:14 +0200

    *

--- api/install/api_allspark/index.php
+++ api/install/api_allspark/index.php
@@ -1,7 +1,5 @@
 <?php
-if ($path[2] == 'auth')
-  include_once 'auth.php';
-else if ($path[2] == 'login')
+if ($path[2] == 'login')
   include_once 'login.php';
 else if ($path[2] == 'status')
   include_once 'status.php';
--- api/install/api_allspark/status.php
+++ api/install/api_allspark/status.php
@@ -30,18 +30,26 @@ $status['apache']['status'] = get_status('apache2');
 $status['apache']['version'] = get_version('apache2');
 $status['php']['status'] = get_status('php');
 $status['php']['version'] = get_version('php');
-$status['mariadb']['status'] = get_status('mariadb');
-$status['mariadb']['version'] = get_version('mariadb');
+$status['mariadb']['status'] = get_status('mariadb-server');
+$status['mariadb']['version'] = get_version('mariadb-server');
 $status['postfix']['status'] = get_status('postfix');
 $status['postfix']['version'] = get_version('postfix');
-$status['dovecot-core']['status'] = get_status('dovecot-core');
-$status['dovecot-core']['version'] = get_version('dovecot-core');
+$status['dovecot']['status'] = get_status('dovecot');
+$status['dovecot']['version'] = get_version('dovecot-core');
+$status['spamassassin']['status'] = get_status('spamassassin');
+$status['spamassassin']['version'] = get_version('spamassassin');
 $status['spamass-milter']['status'] = get_status('spamass-milter');
 $status['spamass-milter']['version'] = get_version('spamass-milter');
 $status['clamav-milter']['status'] = get_status('clamav-milter');
 $status['clamav-milter']['version'] = get_version('clamav-milter');
 $status['rdiff-backup']['status'] = get_status('rdiff-backup');
 $status['rdiff-backup']['version'] = get_version('rdiff-backup');
+$status['certbot']['status'] = get_status('certbot');
+$status['certbot']['version'] = get_version('certbot');
 
-echo json_encode(array("message" => "", "response" => $status));
+exec("cat /srv/webmail/CHANGELOG | grep 'RELEASE' | head -1 | cut -c 9-", $output)
+$status['certbot']['status'] = get_status('roundcube');
+$status['certbot']['version'] = $output[0];
+
+echo json_encode($status);
 ?>
