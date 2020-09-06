1e56aaf0b9e3825f813dde3cab049bd9f9e73e88
Lionel VEST <lionel.vest@adaris.org>
2020-09-06 04:00:58 +0200

    *

--- api/install/api_allspark/login.php
+++ api/install/api_allspark/login.php
@@ -1,4 +1,8 @@
 <?php
+header("Access-Control-Allow-Origin: https://www.demoptimus.fr");
+header("Access-Control-Allow-Methods: GET, HEAD, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Accept, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
 include_once 'config.php';
 include_once 'connect.php';
 include_once 'api_allspark/user.php';
@@ -17,7 +21,6 @@ use allspark\JWT\JWT;
 if($email_exists && openssl_encrypt($data->password, 'aes-128-ecb', $aes_key) == base64_encode($user->password))
 {
     http_response_code(200);
-
     $jwt = new JWT($sha_key, 'HS512', 3600, 10);
     $token = $jwt->encode(["user" => array("id" => $user->id, "email" => $user->email), "aud" => "http://$domain", "scopes" => ['user'], "iss" => "http://$domain"]);
     echo json_encode(array("message" => "Successful login", "token" => $token));
