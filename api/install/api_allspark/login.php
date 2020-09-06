6c11afdd01d306049ba5c417d74f9256a22b40b9
Lionel VEST <lionel.vest@adaris.org>
2020-09-06 04:30:03 +0200

    *

--- api/install/api_allspark/login.php
+++ api/install/api_allspark/login.php
@@ -4,37 +4,34 @@ header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Headers: Content-Type, Accept, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-if ($_SERVER['REQUEST_METHOD'] == "OPTIONS")
+if ($_SERVER['REQUEST_METHOD'] == "POST")
 {
-  http_response_code(200);
-  exit;
-}
+  include_once 'config.php';
+  include_once 'connect.php';
+  include_once 'api_allspark/user.php';
 
-include_once 'config.php';
-include_once 'connect.php';
-include_once 'api_allspark/user.php';
+  $database = new Database();
+  $db = $database->getConnection();
+  $user = new User($db);
+  $data = json_decode(file_get_contents("php://input"));
 
-$database = new Database();
-$db = $database->getConnection();
-$user = new User($db);
-$data = json_decode(file_get_contents("php://input"));
+  $user->email = $data->email;
+  $email_exists = $user->emailExists();
 
-$user->email = $data->email;
-$email_exists = $user->emailExists();
+  include_once 'JWT.php';
+  use allspark\JWT\JWT;
 
-include_once 'JWT.php';
-use allspark\JWT\JWT;
-
-if ($email_exists && openssl_encrypt($data->password, 'aes-128-ecb', $aes_key) == base64_encode($user->password))
-{
-    http_response_code(200);
-    $jwt = new JWT($sha_key, 'HS512', 3600, 10);
-    $token = $jwt->encode(["user" => array("id" => $user->id, "email" => $user->email), "aud" => "http://$domain", "scopes" => ['user'], "iss" => "http://$domain"]);
-    echo json_encode(array("message" => "Successful login", "token" => $token));
-}
-else
-{
-   http_response_code(401);
-   echo json_encode(array("message" => "Login failed"));
+  if ($email_exists && openssl_encrypt($data->password, 'aes-128-ecb', $aes_key) == base64_encode($user->password))
+  {
+      http_response_code(200);
+      $jwt = new JWT($sha_key, 'HS512', 3600, 10);
+      $token = $jwt->encode(["user" => array("id" => $user->id, "email" => $user->email), "aud" => "http://$domain", "scopes" => ['user'], "iss" => "http://$domain"]);
+      echo json_encode(array("message" => "Successful login", "token" => $token));
+  }
+  else
+  {
+     http_response_code(401);
+     echo json_encode(array("message" => "Login failed"));
+  }
 }
 ?>
