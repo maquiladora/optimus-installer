4995fd67c43ab32bdb365703e884a729fcc36ed7
Lionel VEST <lionel.vest@adaris.org>
2020-09-07 00:50:29 +0200

    *

--- api/install/api_allspark/status.php
+++ api/install/api_allspark/status.php
@@ -22,7 +22,7 @@ function get_status($app)
 function get_version($app)
 {
   exec("dpkg -s " . $app . " | grep '^Version:' | cut -c 10- | cut -f1 -d'-' | cut -f1 -d'+' | cut -f2 -d':'", $output);
-  if ($output[0])
+  if ($output)
     return $output[0];
 }
 
