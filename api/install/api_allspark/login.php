38bdd55c534bc7e4eff7beba347497552e68642d
Lionel VEST <lionel.vest@adaris.org>
2020-09-06 04:22:56 +0200

    *

--- api/install/api_allspark/login.php
+++ api/install/api_allspark/login.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: $_SERVER['HTTP_ORIGIN']");
+header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
 header("Access-Control-Allow-Methods: GET, HEAD, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Headers: Content-Type, Accept, Access-Control-Allow-Headers, Authorization, X-Requested-With");
