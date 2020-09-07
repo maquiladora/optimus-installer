ae0b7b089c0497f2254f449b688d53fcb5382a52
Lionel VEST <lionel.vest@adaris.org>
2020-09-08 00:38:34 +0200

    *

--- api/install/api_allspark/login.php
+++ api/install/api_allspark/login.php
@@ -26,8 +26,7 @@ if ($email_exists && openssl_encrypt($data->password, 'aes-128-ecb', $aes_key) =
     $jwt = new JWT($sha_key, 'HS512', 3600, 10);
     $token = $jwt->encode(["user" => array("id" => $user->id, "email" => $user->email), "aud" => "http://$domain", "scopes" => ['user'], "iss" => "http://$domain"]);
     preg_match("/[^\.\/]+\.[^\.\/]+$/", $_SERVER['HTTP_ORIGIN'],$matches);
-    echo "domain:" . $matches[0];
-    setcookie('token',$token,time()+3600,'/',(isset($_SERVER['HTTP_ORIGIN'])?$_SERVER['HTTP_ORIGIN']:$_SERVER['SERVER_NAME']),true,true);
+    setcookie('token',$token,time()+3600,'/',$matches[0],true,true);
     echo json_encode(array("message" => "Successful login", "token" => $token));
 }
 else
