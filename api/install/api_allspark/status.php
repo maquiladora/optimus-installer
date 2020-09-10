c84d15a0b2476d85dd6f114aef94fcb6155b7087
Lionel VEST <lionel.vest@adaris.org>
2020-09-10 05:05:13 +0200

    *

--- api/install/api_allspark/status.php
+++ api/install/api_allspark/status.php
@@ -1,8 +1,8 @@
 <?php
 header("Access-Control-Allow-Origin: " . (isset($_SERVER['HTTP_ORIGIN'])?$_SERVER['HTTP_ORIGIN']:$_SERVER['SERVER_NAME']));
-header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Methods: GET, OPTIONS");
 header("Access-Control-Allow-Credentials: true");
-header("Access-Control-Allow-Headers: Cookie, Content-Type, Accept, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header("Access-Control-Allow-Headers: Content-Type, Accept, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 if ($_SERVER['REQUEST_METHOD'] == "OPTIONS") die(http_response_code(200));
 
 include_once 'auth.php';
