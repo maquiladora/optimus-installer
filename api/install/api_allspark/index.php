deb9548ed1a7ac7527bd37027503e0ce6db2351f
Lionel VEST <lionel.vest@adaris.org>
2020-09-11 10:29:23 +0200

    *

--- api/install/api_allspark/index.php
+++ api/install/api_allspark/index.php
@@ -1,6 +1,8 @@
 <?php
 if ($path[2] == 'login')
   include_once 'login.php';
+else if ($path[2] == 'logout')
+  include_once 'logout.php';
 else if ($path[2] == 'status')
   include_once 'status.php';
 ?>
