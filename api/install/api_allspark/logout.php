b1d0d143e9f57a7c69fc2e2916b648261f8910d1
Lionel VEST <lionel.vest@adaris.org>
2020-09-11 10:58:12 +0200

    *

--- api/install/api_allspark/logout.php
+++ api/install/api_allspark/logout.php
@@ -8,8 +8,7 @@ if ($_SERVER['REQUEST_METHOD'] == "OPTIONS") die(http_response_code(200));
 
 include_once 'config.php';
 
-$cookie_options = array ('expires' => time() - 3600, 'path' => '/', 'domain' => $domain, 'secure' => true, 'httponly' => true, 'samesite' => 'None');
-setcookie('token', '', $cookie_options);
+setcookie('token', '', $cookie_options, time() - 3600, '/', $domain);
 http_response_code(200);
 echo json_encode(array("message" => "Cookie removed"));
 ?>
