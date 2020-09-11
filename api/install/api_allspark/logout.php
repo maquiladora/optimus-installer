42bcbfabe39895913fb2e967d4c2da6032a3198c
Lionel VEST <lionel.vest@adaris.org>
2020-09-11 10:56:58 +0200

    *

--- api/install/api_allspark/logout.php
+++ api/install/api_allspark/logout.php
@@ -6,6 +6,8 @@ header("Access-Control-Allow-Headers: Content-Type, Accept, Authorization");
 header("Access-Control-Max-Age: 5");
 if ($_SERVER['REQUEST_METHOD'] == "OPTIONS") die(http_response_code(200));
 
+include_once 'config.php';
+
 $cookie_options = array ('expires' => time() - 3600, 'path' => '/', 'domain' => $domain, 'secure' => true, 'httponly' => true, 'samesite' => 'None');
 setcookie('token', '', $cookie_options);
 http_response_code(200);
