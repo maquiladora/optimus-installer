cc83e6fbc6ffb379a9bf353a03935a86152be522
Lionel VEST <lionel.vest@adaris.org>
2020-09-12 00:38:47 +0200

    *

--- cloud/allspark/DAV/Auth/Backend/AbstractBearer.php
+++ cloud/allspark/DAV/Auth/Backend/AbstractBearer.php
@@ -29,7 +29,8 @@ abstract class AbstractBearer implements BackendInterface
             $response
         );
 
-        $bearerToken = $auth->getToken($request);
+        $bearerToken = $auth->getCredentials($request);
+        echo $bearerToken;
         //if (!$bearerToken) {
         //    return [false, "No 'Authorization: Bearer' header found. Either the client didn't send one, or the server is mis-configured"];
         //}
