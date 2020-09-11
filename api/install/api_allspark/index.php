8561447c5680917633c282be3a6edcdfedb56f68
Lionel VEST <lionel.vest@adaris.org>
2020-09-11 11:32:33 +0200

    *

--- api/install/api_allspark/index.php
+++ api/install/api_allspark/index.php
@@ -3,6 +3,8 @@ if ($path[2] == 'login')
   include_once 'login.php';
 else if ($path[2] == 'logout')
   include_once 'logout.php';
+  else if ($path[2] == 'logged')
+    include_once 'logged.php';
 else if ($path[2] == 'status')
   include_once 'status.php';
 ?>
