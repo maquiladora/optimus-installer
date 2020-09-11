bf616d1c78f16df7eea80741b3e1eb5fe01cc93f
Lionel VEST <lionel.vest@adaris.org>
2020-09-12 01:01:39 +0200

    *

--- cloud/allspark/DAV/Auth/Backend/AbstractBearer.php
+++ cloud/allspark/DAV/Auth/Backend/AbstractBearer.php
@@ -32,7 +32,7 @@ abstract class AbstractBearer implements BackendInterface
 
         if (isset($_COOKIE['token']))
         {
-          $payload = (new JWT('$API_SHA_KEY', 'HS512', 3600, 10))->decode($token);
+          $payload = (new JWT('$API_SHA_KEY', 'HS512', 3600, 10))->decode($_COOKIE['token']);
           return [true, $payload['user']->email];
         }
         else
