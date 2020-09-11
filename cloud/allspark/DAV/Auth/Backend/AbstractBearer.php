e5bb4ef60bfe96c5c21598aa22b64404a3de40a6
Lionel VEST <lionel.vest@adaris.org>
2020-09-12 01:48:14 +0200

    *

--- cloud/allspark/DAV/Auth/Backend/AbstractBearer.php
+++ cloud/allspark/DAV/Auth/Backend/AbstractBearer.php
@@ -4,17 +4,14 @@ declare(strict_types=1);
 namespace Allspark\DAV\Auth\Backend;
 
 use Sabre\DAV\Auth\Backend\BackendInterface;
-use Sabre\HTTP;
-use Sabre\HTTP\RequestInterface;
-use Sabre\HTTP\ResponseInterface;
 
 include_once 'JWT.php';
 use allspark\JWT\JWT;
 
 abstract class AbstractBearer implements BackendInterface
 {
-  
-  public function check(RequestInterface $request, ResponseInterface $response)
+
+  public function check()
   {
     if (isset($_COOKIE['token']))
     {
@@ -25,30 +22,9 @@ abstract class AbstractBearer implements BackendInterface
       return [false, "Invalid Token"];
   }
 
-    /**
-     * This method is called when a user could not be authenticated, and
-     * authentication was required for the current request.
-     *
-     * This gives you the opportunity to set authentication headers. The 401
-     * status code will already be set.
-     *
-     * In this case of Bearer Auth, this would for example mean that the
-     * following header needs to be set:
-     *
-     * $response->addHeader('WWW-Authenticate', 'Bearer realm=SabreDAV');
-     *
-     * Keep in mind that in the case of multiple authentication backends, other
-     * WWW-Authenticate headers may already have been set, and you'll want to
-     * append your own WWW-Authenticate header instead of overwriting the
-     * existing one.
-     */
-    public function challenge(RequestInterface $request, ResponseInterface $response)
-    {
-        $auth = new HTTP\Auth\Bearer(
-            $this->realm,
-            $request,
-            $response
-        );
-        $auth->requireLogin();
-    }
+
+  public function challenge()
+  {
+
+  }
 }
