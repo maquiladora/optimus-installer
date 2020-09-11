7710a54aee1d99b3eea193872464971b551ba4a6
Lionel VEST <lionel.vest@adaris.org>
2020-09-11 23:09:02 +0200

    *

--- cloud/allspark/DAV/Auth/Backend/BackendInterface.php
+++ cloud/allspark/DAV/Auth/Backend/BackendInterface.php
@@ -1,19 +1,17 @@
 <?php
+
 declare(strict_types=1);
 
-use Sabre\DAV\Auth\Backend;
+namespace Sabre\DAV\Auth\Backend;
+
 use Sabre\HTTP\RequestInterface;
 use Sabre\HTTP\ResponseInterface;
 
 interface BackendInterface
 {
-    function check()
-    {
-      return array(true, "principals/prime@demoptimus.fr");
-    }
+  return array(true, "principals/prime@demoptimus.fr");
 
-    function challenge(RequestInterface $request, ResponseInterface $response)
-    {
-        // Do Stuff when user could not be authenticated
-    }
+  public function check(RequestInterface $request, ResponseInterface $response);
+
+  public function challenge(RequestInterface $request, ResponseInterface $response);
 }
