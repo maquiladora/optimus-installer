ac0c97a23a0b9404d9ff2690381bd89427082af6
Lionel VEST <lionel.vest@adaris.org>
2020-09-11 10:53:18 +0200

    *

--- api/install/api_allspark/logout.php
+++ api/install/api_allspark/logout.php
@@ -6,9 +6,9 @@ header("Access-Control-Allow-Headers: Content-Type, Accept, Authorization");
 header("Access-Control-Max-Age: 5");
 if ($_SERVER['REQUEST_METHOD'] == "OPTIONS") die(http_response_code(200));
 
-  preg_match("/[^\.\/]+\.[^\.\/]+$/", (isset($_SERVER['HTTP_ORIGIN'])?$_SERVER['HTTP_ORIGIN']:$_SERVER['SERVER_NAME']), $matches);
+preg_match("/[^\.\/]+\.[^\.\/]+$/", (isset($_SERVER['HTTP_ORIGIN'])?$_SERVER['HTTP_ORIGIN']:$_SERVER['SERVER_NAME']), $matches);
 $cookie_options = array ('expires' => time() - 3600, 'path' => '/', 'domain' => $matches[0], 'secure' => true, 'httponly' => true, 'samesite' => 'None');
-setcookie('token', $token, $cookie_options);
+setcookie('token', '', $cookie_options);
 http_response_code(200);
 echo json_encode(array("message" => "Cookie removed"));
 ?>
