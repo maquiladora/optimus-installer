556599765fe699465e05b27b999abb7c9943c1b3
Lionel VEST <lionel.vest@adaris.org>
2020-09-11 10:27:55 +0200

    *

--- api/install/api_allspark/logout.php
+++ api/install/api_allspark/logout.php
@@ -1,6 +1,6 @@
 <?php
 header("Access-Control-Allow-Origin: " . (isset($_SERVER['HTTP_ORIGIN'])?$_SERVER['HTTP_ORIGIN']:$_SERVER['SERVER_NAME']));
-header("Access-Control-Allow-Methods: GET, OPTIONS");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Headers: Content-Type, Accept, Authorization");
 header("Access-Control-Max-Age: 5");
