0739c3960dd35f3fc13316618e40839fd46401e0
Lionel VEST <lionel.vest@adaris.org>
2020-09-12 02:30:37 +0200

    *

--- cloud/allspark/DAV/Auth/Backend/JWT.php
+++ cloud/allspark/DAV/Auth/Backend/JWT.php
@@ -8,7 +8,7 @@ use Sabre\HTTP\ResponseInterface;
 include_once 'JWT.php';
 use allspark\JWT\JWT;
 
-class JWT_PDO extends \Sabre\DAV\Auth\Backend\AbstractBearer
+class JWT extends \Sabre\DAV\Auth\Backend\AbstractBearer
 {
 
 	public function validateBearerToken($bearerToken)
