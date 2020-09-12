faa56c387b9d47c5197c6a2e3f5dced829a37fdf
Lionel VEST <lionel.vest@adaris.org>
2020-09-12 02:22:09 +0200

    *

--- cloud/allspark/DAV/Auth/Backend/JWT_PDO.php
+++ cloud/allspark/DAV/Auth/Backend/JWT_PDO.php
@@ -2,7 +2,36 @@
 
 namespace Allspark\DAV\Auth\Backend;
 
-class JWT_PDO extends \Allspark\DAV\Auth\Backend\AbstractBearer
-{
+include_once 'JWT.php';
+use allspark\JWT\JWT;
 
+class JWT_PDO extends \Sabre\DAV\Auth\Backend\AbstractBearer
+{
+	public function check(RequestInterface $request, ResponseInterface $response)
+	{
+		if (isset($_COOKIE['token']))
+		{
+			$payload = (new JWT('$API_SHA_KEY', 'HS512', 3600, 10))->decode($_COOKIE['token']);
+			return [true, "principals/".$payload['user']->email];
+		}
+		else
+			return [false, "Invalid Token"];
+	}
+
+	public function challenge(RequestInterface $request, ResponseInterface $response)
+	{
+		$auth = new HTTP\Auth\Digest(
+					 '',
+					 $request,
+					 $response
+			 );
+			 $auth->init();
+
+			 $oldStatus = $response->getStatus() ?: 200;
+			 $auth->requireLogin();
+
+			 // Preventing the digest utility from modifying the http status code,
+			 // this should be handled by the main plugin.
+			 $response->setStatus($oldStatus);
+	}
 }
