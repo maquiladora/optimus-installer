85649771e8c00c90f00e37aa0ce6b3ab9edcb85f
Lionel VEST <lionel.vest@adaris.org>
2020-09-13 00:42:05 +0200

    *

--- www/index.php
+++ www/index.php
@@ -1,44 +1,16 @@
+<?php
+  if (!isset($_COOKIE['token'])
+    include('login.php');
+?>
+
 <html>
   <body>
-    <div id="container" style="text-align:center">
-      <?='$DOMAIN'?><br/><br>
-      <label for="email">Email :</label><br><input id="email" type="text" onkeypress="if (event.keyCode === 13) login()"/>
-      <br/><br/>
-      <label for="password">Password :</label><br><input id="password" type="password" onkeypress="if (event.keyCode === 13) login()"/>
-      <br/><br/>
-      <input type="button" value="login" onclick="login()"/>
-      <br/><br/>
-    </div>
+
+      <input type="button" style="position:fixed;top:5px;right:5px" value="login" onclick="logout()"/>
+
   </body>
 
   <script>
-
-  function login()
-  {
-    fetch('https://api.$DOMAIN/allspark/login',
-		{
-			headers: {'Accept': 'application/json', 'Content-Type': 'application/json'},
-			method: "POST",
-			credentials: "include",
-      body: JSON.stringify({"email": document.getElementById('email').value, "password": document.getElementById('password').value})
-		})
-		.then(function(response)
-		{
-      if (response.status === 200)
-      {
-        parent.postMessage('logged','*');
-        window.location.reload();
-      }
-      else
-        return response.json();
-		})
-    .then(function(response)
-    {
-      if (response.message)
-        alert(response.message);
-    })
-		.catch(error => console.log("Erreur : " + error));
-  }
 
   function logout()
   {
@@ -58,32 +30,6 @@
 		})
 		.catch(error => console.log("Erreur : " + error));
   }
-
-  function logged()
-  {
-    fetch('https://api.$DOMAIN/allspark/logged',
-    {
-      headers: {'Accept': 'application/json', 'Content-Type': 'application/json'},
-      method: 'POST',
-      credentials: "include"
-    })
-		.then(function(response)
-		{
-      if (response.status === 200)
-      {
-        document.getElementById('container').innerHTML='';
-        logout_button = document.createElement('input');
-        logout_button.type = 'button';
-        logout_button.value = 'logout';
-        logout_button.onclick = function(){logout()};
-        document.getElementById('container').appendChild(logout_button);
-      }
-		})
-		.catch(error => console.log("Erreur : " + error));
-  }
-
-  document.getElementById('email').focus();
-  logged();
 
   </script>
 </html>
---
+++ www/login.php
@@ -0,0 +1,45 @@
+<html>
+
+  <body>
+    <?='$DOMAIN'?><br/><br>
+    <label for="email">Email :</label><br><input id="email" type="text" onkeypress="if (event.keyCode === 13) login()"/>
+    <br/><br/>
+    <label for="password">Password :</label><br><input id="password" type="password" onkeypress="if (event.keyCode === 13) login()"/>
+    <br/><br/>
+    <input type="button" value="login" onclick="login()"/>
+    <br/><br/>
+  </body>
+
+  <script>
+  function login()
+  {
+    fetch('https://api.$DOMAIN/allspark/login',
+		{
+			headers: {'Accept': 'application/json', 'Content-Type': 'application/json'},
+			method: "POST",
+			credentials: "include",
+      body: JSON.stringify({"email": document.getElementById('email').value, "password": document.getElementById('password').value})
+		})
+		.then(function(response)
+		{
+      if (response.status === 200)
+      {
+        parent.postMessage('logged','*');
+        window.location.reload();
+      }
+      else
+        return response.json();
+		})
+    .then(function(response)
+    {
+      if (response.message)
+        alert(response.message);
+    })
+		.catch(error => console.log("Erreur : " + error));
+  }
+  document.getElementById('email').focus();
+  </script>
+
+</html>
+
+<?php exit; ?>
