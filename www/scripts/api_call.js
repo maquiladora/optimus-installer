7042e73361faf8d826b1b0bf0d51d582d321284f
Lionel VEST <lionel.vest@adaris.org>
2020-09-13 10:23:44 +0200

    *

--- www/scripts/api_call.js
+++ www/scripts/api_call.js
@@ -20,7 +20,7 @@ function api_call(endpoint, method, data)
       return setTimeout("api_call('" + endpoint + "', '" + method + "', '" + data + "')",500);
     }
     else
-      return response;
+      return JSON.stringify(response);
   })
   .catch(error => console.log("Error : " + error));
 
