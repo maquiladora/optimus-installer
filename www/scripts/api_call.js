23b45b3f65466b4ff9844d118792f1be770e88de
Lionel VEST <lionel.vest@adaris.org>
2020-09-13 09:41:45 +0200

    *

--- www/scripts/api_call.js
+++ www/scripts/api_call.js
@@ -3,6 +3,10 @@ function api_call(endpoint, method, data)
   if (typeof login_iframe === 'object')
     return setTimeout("api_call('" + endpoint + "', '" + method + "', '" + data + "')",500);
 
+  domain = domain.split('.');
+  domain = domain[domain.length-2] + '.' + domain[domain.length-1];
+  alert(domain);
+
   var fetch_options = {headers: {'Accept': 'application/json', 'Content-Type': 'application/json'}, method: method, credentials: "include"};
   if (method != 'GET')
     fetch_options.push = {body: data};
