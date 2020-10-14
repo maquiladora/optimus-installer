f3fef2c904e11591df0ea6712d8a6dacf333f0d1
Lionel VEST <lionel.vest@adaris.org>
2020-10-14 15:02:03 +0200

    *

--- api/install/api_optimus/classes/dossier.php
+++ api/install/api_optimus/classes/dossier.php
@@ -41,7 +41,7 @@ class dossier
     $stmt->bindParam(':numero', $this->numero);
     $stmt->bindParam(':date_ouverture', $this->date_ouverture);
     if($stmt->execute())
-      return $this->conn->lastInsertId();
+      return $this;
     else
       return $stmt->errorInfo()[2];
   }
