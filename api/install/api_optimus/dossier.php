4c2346241b86c95dd319d71002f7e36258816cad
Lionel VEST <lionel.vest@adaris.org>
2020-10-14 14:41:51 +0200

    *

--- api/install/api_optimus/dossier.php
+++ api/install/api_optimus/dossier.php
@@ -1,6 +1,6 @@
 <?php
 header("Access-Control-Allow-Origin: " . (isset($_SERVER['HTTP_ORIGIN'])?$_SERVER['HTTP_ORIGIN']:$_SERVER['SERVER_NAME']));
-header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Allow-Methods: POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Headers: Content-Type, Accept, Authorization");
 header("Access-Control-Max-Age: 5");
