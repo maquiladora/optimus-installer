12b6ef0926657d22594b4cc3c34329a482e6b44e
Lionel VEST <lionel.vest@adaris.org>
2020-10-14 20:51:12 +0200

    *

--- api/install/api_optimus/classes/dossier.php
+++ api/install/api_optimus/classes/dossier.php
@@ -51,9 +51,9 @@ class dossier
     $old_name = $this->conn->query("SELECT nom FROM " . $this->table_name . " WHERE id = '" . $data->id . "'")->fetch();
     $new_name = $this->conn->query("UPDATE " . $this->table_name . " SET nom = '" . $data->new_name . "' WHERE id = '" . $data->id . "'")->fetch();
     @rename('/srv/files/prime@demoptimus.fr/==DOSSIERS==/' . $old_name['nom'], '/srv/files/prime@demoptimus.fr/==DOSSIERS==/' . $data->new_name);
-    @rename('/srv/mailboxes/prime@demoptimus.fr/==DOSSIERS==/' . $old_name['nom'], '/srv/mailboxes/prime@demoptimus.fr/==DOSSIERS==/' . mb_convert_encoding($data->new_name, "UTF7-IMAP","UTF-8"));
+    @rename('/srv/mailboxes/prime@demoptimus.fr/==DOSSIERS==/' . mb_convert_encoding($old_name['nom'], "UTF7-IMAP","UTF-8"), '/srv/mailboxes/prime@demoptimus.fr/==DOSSIERS==/' . mb_convert_encoding($data->new_name, "UTF7-IMAP","UTF-8"));
     $subscriptions = file_get_contents('/srv/mailboxes/prime@demoptimus.fr/subscriptions');
-    $subscriptions = str_replace('==DOSSIERS==/' . $old_name['nom'], '==DOSSIERS==/' . mb_convert_encoding($data->new_name, "UTF7-IMAP","UTF-8"), $subscriptions);
+    $subscriptions = str_replace('==DOSSIERS==/' . mb_convert_encoding($old_name['nom'], "UTF7-IMAP","UTF-8"), '==DOSSIERS==/' . mb_convert_encoding($data->new_name, "UTF7-IMAP","UTF-8"), $subscriptions);
     @file_put_contents('/srv/mailboxes/prime@demoptimus.fr/subscriptions', $subscriptions);
     return true;
   }
