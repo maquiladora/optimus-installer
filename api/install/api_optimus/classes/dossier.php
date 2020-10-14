e72af503483b5dcb00931841c009b51dea8b5974
Lionel VEST <lionel.vest@adaris.org>
2020-10-14 09:59:18 +0200

    *

--- api/install/api_optimus/classes/dossier.php
+++ api/install/api_optimus/classes/dossier.php
@@ -17,33 +17,29 @@ class dossier
 
   function create()
   {
-    $query = "INSERT INTO " . $this->table_name . " SET d = :id, nom = :nom, numero = :numero, date_ouverture = :date_ouverture";
+    $query = "INSERT INTO " . $this->table_name . " SET id = :id, nom = :nom, numero = :numero, date_ouverture = :date_ouverture";
 
-    try
+    $stmt = $this->conn->prepare($query);
+
+    $this->id=htmlspecialchars(strip_tags('500'));
+    $this->nom=htmlspecialchars(strip_tags('testapi'));
+    $this->numero=htmlspecialchars(strip_tags('20/030'));
+    $this->date_ouverture=htmlspecialchars(strip_tags('2020-10-14'));
+
+    $stmt->bindParam(':id', $this->id);
+    $stmt->bindParam(':nom', $this->nom);
+    $stmt->bindParam(':numero', $this->numero);
+    $stmt->bindParam(':date_ouverture', $this->date_ouverture);
+
+    if($stmt->execute())
     {
-      $stmt = $this->conn->prepare($query);
-
-      $this->id=htmlspecialchars(strip_tags('500'));
-      $this->nom=htmlspecialchars(strip_tags('testapi'));
-      $this->numero=htmlspecialchars(strip_tags('20/030'));
-      $this->date_ouverture=htmlspecialchars(strip_tags('2020-10-14'));
-
-      $stmt->bindParam(':id', $this->id);
-      $stmt->bindParam(':nom', $this->nom);
-      $stmt->bindParam(':numero', $this->numero);
-      $stmt->bindParam(':date_ouverture', $this->date_ouverture);
-
-      if($stmt->execute())
-      {
-          return true;
-          return $this->conn->errorInfo();
-      }
-      return false;
+        return true;
     }
-    catch(Exception $e)
-    {
-      return $e->getMessage();
-    }
+
+    return $stmt->errorInfo();
+
+
+    return false;
   }
 }
 ?>
