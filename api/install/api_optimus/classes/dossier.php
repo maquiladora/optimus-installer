5f38789b9bc8f51f793feff4547f97b041945d82
Lionel VEST <lionel.vest@adaris.org>
2020-10-15 01:03:57 +0200

    *

--- api/install/api_optimus/classes/dossier.php
+++ api/install/api_optimus/classes/dossier.php
@@ -2,7 +2,6 @@
 class dossier
 {
   private $conn;
-  private $table_name = "dossiers";
 
   public $id;
   public $nom;
@@ -17,7 +16,7 @@ class dossier
 
   function create($data)
   {
-    $last_numero = $this->conn->query("SELECT numero FROM `" . $data->db . "`." . $this->table_name . " WHERE numero LIKE '" . date('y') . "/%' ORDER BY id DESC LIMIT 1")->fetch();
+    $last_numero = $this->conn->query("SELECT numero FROM `" . $data->db . "`.dossiers WHERE numero LIKE '" . date('y') . "/%' ORDER BY id DESC LIMIT 1")->fetch();
     $this->nom = time();
     $this->numero = date('y') . '/' . str_pad(intval(substr($last_numero['numero'],3))+1, 4, "0", STR_PAD_LEFT);
     $this->date_ouverture = date('Y-m-d');
@@ -32,7 +31,7 @@ class dossier
     @chgrp('/srv/mailboxes/' . $data->db . '/==DOSSIERS==/'.$this->nom.'/tmp', 'mailboxes');
     @file_put_contents('/srv/mailboxes/' . $data->db . '/subscriptions',"==DOSSIERS==/" . $this->nom . "\n", FILE_APPEND);
 
-    $stmt = $this->conn->prepare("INSERT INTO `" . $data->db . "`." . $this->table_name . " SET nom = :nom, numero = :numero, date_ouverture = :date_ouverture");
+    $stmt = $this->conn->prepare("INSERT INTO `" . $data->db . "`.dossiers SET nom = :nom, numero = :numero, date_ouverture = :date_ouverture");
     $stmt->bindParam(':nom', $this->nom);
     $stmt->bindParam(':numero', $this->numero);
     $stmt->bindParam(':date_ouverture', $this->date_ouverture);
@@ -48,8 +47,8 @@ class dossier
 
   function rename($data)
   {
-    $old_name = $this->conn->query("SELECT nom FROM `" . $data->db . "`." . $this->table_name . " WHERE id = '" . $data->id . "'")->fetch();
-    $new_name = $this->conn->query("UPDATE `" . $data->db . "`." . $this->table_name . " SET nom = '" . $data->new_name . "' WHERE id = '" . $data->id . "'")->fetch();
+    $old_name = $this->conn->query("SELECT nom FROM `" . $data->db . "`.dossiers WHERE id = '" . $data->id . "'")->fetch();
+    $new_name = $this->conn->query("UPDATE `" . $data->db . "`.dossiers SET nom = '" . $data->new_name . "' WHERE id = '" . $data->id . "'")->fetch();
     @rename('/srv/files/' . $data->db . '/==DOSSIERS==/' . $old_name['nom'], '/srv/files/' . $data->db . '/==DOSSIERS==/' . $data->new_name);
     @rename('/srv/mailboxes/' . $data->db . '/==DOSSIERS==/' . mb_convert_encoding($old_name['nom'], "UTF7-IMAP","UTF-8"), '/srv/mailboxes/' . $data->db . '/==DOSSIERS==/' . mb_convert_encoding($data->new_name, "UTF7-IMAP","UTF-8"));
     $subscriptions = file_get_contents('/srv/mailboxes/' . $data->db . '/subscriptions');
@@ -61,18 +60,18 @@ class dossier
 
   function delete($data)
   {
-    $dossier = $this->conn->query("SELECT nom FROM `" . $data->db . "`." . $this->table_name . " WHERE id = '" . $data->id . "'")->fetch();
+    $dossier = $this->conn->query("SELECT nom FROM `" . $data->db . "`.dossiers WHERE id = '" . $data->id . "'")->fetch();
 
     $interventions_exists = $this->conn->query("SELECT id FROM `" . $data->db . "`.interventions WHERE dossier = '" . $data->id . "'")->rowCount();
     if ($interventions_exists > 0)
-      return "Ce dossier ne peut pas être supprimé car il contient des fiches d'intervention";
+      return array("code" => 400, "message" => "Ce dossier ne peut pas être supprimé car il contient des fiches d'intervention");
 
     $factures_exists = $this->conn->query("SELECT id FROM optimus_user_1.interventions WHERE dossier = '" . $data->id . "' AND db IS NOT NULL")->rowCount();
     if ($factures_exists > 0)
-      return "Ce dossier ne peut pas être supprimé car des factures le concernant ont été émises";
+      return array("code" => 400, "message" => "Ce dossier ne peut pas être supprimé car des factures le concernant ont été émises");
 
-    $dossier_delete = $this->conn->query("DELETE FROM `" . $data->db . "`." . $this->table_name . " WHERE id = '" . $data->id . "'");
-    $intervenants_delete = $this->conn->query("DELETE FROM `" . $data->db . "`." . $this->table_name . ".dossiers_intervenants WHERE dossier = '" . $data->id . "'");
+    $dossier_delete = $this->conn->query("DELETE FROM `" . $data->db . "`.dossiers WHERE id = '" . $data->id . "'");
+    $intervenants_delete = $this->conn->query("DELETE FROM `" . $data->db . "`.dossiers_intervenants WHERE dossier = '" . $data->id . "'");
 
     @rmdir('/srv/files/' . $data->db . '/==DOSSIERS==/' . $dossier['nom']);
     @rmdir('/srv/mailboxes/' . $data->db . '/==DOSSIERS==/' . mb_convert_encoding($dossier['nom'], "UTF7-IMAP","UTF-8"));
