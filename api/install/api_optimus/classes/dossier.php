04c5071cb6ea210cb271f92a39f88764935b25c1
Lionel VEST <lionel.vest@adaris.org>
2020-10-14 10:58:27 +0200

    *

--- api/install/api_optimus/classes/dossier.php
+++ api/install/api_optimus/classes/dossier.php
@@ -17,11 +17,20 @@ class dossier
 
   function create()
   {
-    $last_numero = $this->conn->query("SELECT numero FROM " . $this->table_name . " WHERE numero LIKE '" . date('y') . "/%' ORDER BY id DESC LIMIT 1")->fetch();
-    $stmt = $this->conn->prepare("INSERT INTO " . $this->table_name . " SET nom = :nom, numero = :numero, date_ouverture = :date_ouverture");
     $this->nom = time();
     $this->numero = date('y') . '/' . str_pad(intval(substr($last_numero['numero'],3))+1, 4, "0", STR_PAD_LEFT);
     $this->date_ouverture = date('Y-m-d');
+
+    //CREATION DU DOSSIER INFORMATIQUE
+    mkdir('/srv/files/prime@demoptimus.fr/==DOSSIERS==/'.$this->nom);
+
+    //CREATION DU DOSSIER IMAP
+    mkdir('/srv/mailboxes/prime@demoptimus.fr/==DOSSIERS==/'.$this->nom);
+
+    //INSERT DANS LA BASE DE DONNEE
+    $last_numero = $this->conn->query("SELECT numero FROM " . $this->table_name . " WHERE numero LIKE '" . date('y') . "/%' ORDER BY id DESC LIMIT 1")->fetch();
+    $stmt = $this->conn->prepare("INSERT INTO " . $this->table_name . " SET nom = :nom, numero = :numero, date_ouverture = :date_ouverture");
+
     $stmt->bindParam(':nom', $this->nom);
     $stmt->bindParam(':numero', $this->numero);
     $stmt->bindParam(':date_ouverture', $this->date_ouverture);
@@ -29,8 +38,6 @@ class dossier
       return $this->conn->lastInsertId();
     else
       return $stmt->errorInfo()[2];
-
-    return false;
   }
 }
 ?>
