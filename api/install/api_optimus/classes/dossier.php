57335f963a55eb27610d4aa8030b24da4d237158
Lionel VEST <lionel.vest@adaris.org>
2020-10-14 10:55:04 +0200

    *

--- api/install/api_optimus/classes/dossier.php
+++ api/install/api_optimus/classes/dossier.php
@@ -17,7 +17,7 @@ class dossier
 
   function create()
   {
-    $last_numero = $this->conn->query("SELECT numero FROM " . $this->table_name . " WHERE numero LIKE '" . date('Y') . "/%' ORDER BY id DESC LIMIT 1")->fetch();
+    $last_numero = $this->conn->query("SELECT numero FROM " . $this->table_name . " WHERE numero LIKE '" . date('y') . "/%' ORDER BY id DESC LIMIT 1")->fetch();
     $stmt = $this->conn->prepare("INSERT INTO " . $this->table_name . " SET nom = :nom, numero = :numero, date_ouverture = :date_ouverture");
     $this->nom = time();
     $this->numero = date('y') . '/' . str_pad(intval(substr($last_numero['numero'],3))+1, 4, "0", STR_PAD_LEFT);
