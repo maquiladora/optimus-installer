38f94150fe4ba7c84e700e2bb9e97f1a3381fad8
Lionel VEST <lionel.vest@adaris.org>
2020-10-16 00:25:17 +0200

    *

--- optimus/optimus_user/2020-08-01.sql
+++ optimus/optimus_user/2020-08-01.sql
@@ -2,58 +2,67 @@ CREATE DATABASE IF NOT EXISTS `optimus_user_1` CHARACTER SET utf8mb4 COLLATE utf
 USE `optimus_user_1`;
 
 CREATE TABLE IF NOT EXISTS `agendas` (
-  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `id` smallint unsigned NOT NULL AUTO_INCREMENT,
   `nom` varchar(32) DEFAULT NULL,
   `couleur` varchar(6) DEFAULT 'FF0000',
-  `affichage` bit(1) NOT NULL DEFAULT b'1',
+  `affichage` bit NOT NULL DEFAULT b'1',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4;
 
 
 CREATE TABLE IF NOT EXISTS `agendas_evenements` (
-  `id` int(10) unsigned NOT NULL,
+  `id` int unsigned NOT NULL,
   `description` varchar(128) DEFAULT NULL,
   `debut` datetime DEFAULT NULL,
   `fin` datetime DEFAULT NULL,
-  `allday` bit(1) NOT NULL,
+  `allday` bit NOT NULL,
   `lieu` varchar(128) DEFAULT NULL,
-  `dossier` int(11) DEFAULT NULL,
+  `dossier` int unsigned DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4;
 
 
+CREATE TABLE IF NOT EXISTS `authorizations` (
+  `email` varchar(128) DEFAULT NULL,
+  `resource` varchar(128) DEFAULT NULL,
+  `read` bit NOT NULL DEFAULT 0,
+  `write` bit NOT NULL DEFAULT 0,
+  PRIMARY KEY (`email`,`resource`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4;
+
+
 CREATE TABLE IF NOT EXISTS `contacts` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `type` tinyint(4) unsigned NOT NULL DEFAULT 30,
-  `title` tinyint(4) unsigned NOT NULL DEFAULT 0,
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `type` tinyint unsigned NOT NULL DEFAULT 30,
+  `title` tinyint unsigned NOT NULL DEFAULT 0,
   `firstname` varchar(255) DEFAULT '',
   `lastname` varchar(255) DEFAULT '',
   `birth_date` date DEFAULT NULL,
   `birth_zipcode` varchar(255) DEFAULT NULL,
   `birth_city` varchar(255) DEFAULT NULL,
   `birth_city_name` varchar(255) DEFAULT NULL,
-  `birth_country` int(11) unsigned NOT NULL DEFAULT 0,
+  `birth_country` int unsigned NOT NULL DEFAULT 0,
   `insee` varchar(255) DEFAULT NULL,
-  `marital_status` tinyint(4) unsigned NOT NULL DEFAULT 0,
+  `marital_status` tinyint unsigned NOT NULL DEFAULT 0,
   `maiden_name` varchar(255) DEFAULT NULL,
   `company_name` varchar(255) DEFAULT '',
-  `company_type` smallint(5) unsigned NOT NULL DEFAULT 0,
+  `company_type` smallint unsigned NOT NULL DEFAULT 0,
   `company_capital` varchar(255) DEFAULT NULL,
   `rcs` varchar(255) DEFAULT NULL,
   `siret` varchar(255) DEFAULT NULL,
-  `company_registration_city` smallint(5) unsigned NOT NULL DEFAULT '0',
-  `company_representative` tinyint(4) unsigned NOT NULL DEFAULT 0,
+  `company_registration_city` smallint unsigned NOT NULL DEFAULT '0',
+  `company_representative` tinyint unsigned NOT NULL DEFAULT 0,
   `address` varchar(255) DEFAULT NULL,
   `zipcode` varchar(255) DEFAULT NULL,
   `city` varchar(255) DEFAULT NULL,
   `city_name` varchar(255) DEFAULT NULL,
-  `country` int(11) unsigned NOT NULL DEFAULT 74,
+  `country` int unsigned NOT NULL DEFAULT 74,
   `phone` varchar(255) DEFAULT NULL,
   `fax` varchar(255) DEFAULT NULL,
   `mobile` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   `website` varchar(255) DEFAULT NULL,
-  `company` int(11) unsigned NOT NULL DEFAULT 0,
+  `company` int unsigned NOT NULL DEFAULT 0,
   `job_title` varchar(255) DEFAULT NULL,
   `pro_phone` varchar(255) DEFAULT NULL,
   `pro_fax` varchar(255) DEFAULT NULL,
@@ -64,17 +73,17 @@ CREATE TABLE IF NOT EXISTS `contacts` (
   `bic` varchar(255) DEFAULT NULL,
   `tva` varchar(255) DEFAULT NULL,
   `notes` text DEFAULT NULL,
-  `categorie` tinyint(4) unsigned DEFAULT 0,
+  `categorie` tinyint unsigned DEFAULT 0,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4;
 
 
 CREATE TABLE IF NOT EXISTS `dossiers_intervenants` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `dossier` int(11) unsigned NOT NULL,
-  `contact` int(11) unsigned NOT NULL,
-  `qualite` smallint(5) unsigned NOT NULL,
-  `lien` int(11) unsigned NOT NULL,
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `dossier` int unsigned NOT NULL,
+  `contact` int unsigned NOT NULL,
+  `qualite` smallint unsigned NOT NULL,
+  `lien` int unsigned NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4;
 
@@ -85,12 +94,12 @@ CREATE TABLE IF NOT EXISTS `dossiers` (
   `nom` varchar(255) DEFAULT NULL,
   `date_ouverture` date DEFAULT NULL,
   `date_classement` date DEFAULT NULL,
-  `numero_archive` int(11) DEFAULT NULL,
-  `domaine` tinyint(4) NOT NULL DEFAULT 0,
-  `sous_domaine` tinyint(4) NOT NULL DEFAULT 0,
-  `conseil` bit(1) NOT NULL DEFAULT b'0',
-  `contentieux` bit(1) NOT NULL DEFAULT b'0',
-  `aj` bit(1) NOT NULL DEFAULT b'0',
+  `numero_archive` int unsigned DEFAULT NULL,
+  `domaine` tinyint unsigned NOT NULL DEFAULT 0,
+  `sous_domaine` tinyint unsigned NOT NULL DEFAULT 0,
+  `conseil` bit NOT NULL DEFAULT b'0',
+  `contentieux` bit NOT NULL DEFAULT b'0',
+  `aj` bit NOT NULL DEFAULT b'0',
   `notes` text DEFAULT NULL,
   `rg` varchar(8) DEFAULT NULL,
   `portalis` varchar(16) DEFAULT NULL,
@@ -99,25 +108,25 @@ CREATE TABLE IF NOT EXISTS `dossiers` (
 
 
 CREATE TABLE IF NOT EXISTS `fax` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
   `uid` varchar(8) DEFAULT '',
   `recipient` varchar(15) DEFAULT NULL,
   `sender` varchar(15) DEFAULT NULL,
-  `dossier` int(11) DEFAULT NULL,
+  `dossier` int unsigned DEFAULT NULL,
   `filename` varchar(255) DEFAULT NULL,
   `status` varchar(8) DEFAULT NULL,
   `status_message` varchar(255) DEFAULT NULL,
   `date_sent` datetime DEFAULT NULL,
   `date_updated` datetime DEFAULT NULL,
-  `pages` smallint(6) DEFAULT NULL,
-  `pages_sent` smallint(6) DEFAULT NULL,
+  `pages` smallint unsigned DEFAULT NULL,
+  `pages_sent` smallint unsigned DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4;
 
 
 CREATE TABLE IF NOT EXISTS `interventions` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `dossier` int(11) NOT NULL DEFAULT 0,
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `dossier` int unsigned NOT NULL DEFAULT 0,
   `date_ouverture` date DEFAULT NULL,
   `description` varchar(255) NOT NULL,
   `db` varchar(16) DEFAULT NULL,
@@ -129,15 +138,15 @@ CREATE TABLE IF NOT EXISTS `interventions` (
 
 
 CREATE TABLE IF NOT EXISTS `interventions_diligences` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `intervention` int(11) NOT NULL DEFAULT 0,
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `intervention` int unsigned NOT NULL DEFAULT 0,
   `date` date DEFAULT NULL,
-  `intervenant` int(11) NOT NULL DEFAULT 0,
-  `compte` int(11) NOT NULL DEFAULT 0,
+  `intervenant` int unsigned NOT NULL DEFAULT 0,
+  `compte` int unsigned NOT NULL DEFAULT 0,
   `commission` decimal(2,2) NOT NULL DEFAULT 0.00,
   `description` text DEFAULT NULL,
-  `categorie` tinyint(4) NOT NULL DEFAULT 0,
-  `type` tinyint(4) NOT NULL DEFAULT 0,
+  `categorie` tinyint unsigned NOT NULL DEFAULT 0,
+  `type` tinyint unsigned NOT NULL DEFAULT 0,
   `coefficient` decimal(5,2) NOT NULL DEFAULT 0.00,
   `tarif` decimal(10,2) NOT NULL DEFAULT 0.00,
   PRIMARY KEY (`id`) USING BTREE
@@ -152,8 +161,8 @@ CREATE TABLE IF NOT EXISTS `settings` (
 
 
 CREATE TABLE IF NOT EXISTS `structures` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `structure` int(11) unsigned DEFAULT NULL,
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `structure` varchar(128) DEFAULT NULL,
   `server` varchar(128) DEFAULT NULL,
   `entree` date DEFAULT NULL,
   `sortie` date DEFAULT NULL,
