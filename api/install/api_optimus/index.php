e6d1085113ae1a5d8411eac04b05700db06452cb
Lionel VEST <lionel.vest@adaris.org>
2020-10-17 16:51:47 +0200

    *

--- api/install/api_optimus/index.php
+++ api/install/api_optimus/index.php
@@ -3,7 +3,7 @@ if ($path[2] == 'contact')
   include_once 'contact.php';
 else if ($path[2] == 'dossier')
   include_once 'dossier.php';
-else if ($path[3] == 'authorization')
+else if ($path[2] == 'authorization')
   include_once 'authorization.php';
 
 ?>
