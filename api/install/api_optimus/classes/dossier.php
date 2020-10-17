e212bfb064efabbd09449b538bea290dcbd4af76
Lionel VEST <lionel.vest@adaris.org>
2020-10-17 15:56:52 +0200

    *

--- api/install/api_optimus/classes/dossier.php
+++ api/install/api_optimus/classes/dossier.php
@@ -76,7 +76,8 @@ class dossier
   {
     if (!preg_match("/^[a-z0-9_@.]+$/", $data->db)) return array("code" => 400, "message" => "Base de données invalide");
     if (!preg_match("/^\d+$/", $data->id)) return array("code" => 400, "message" => "Identifiant invalide");
-    if ($data->new_name == '.' OR $data->new_name = '..') return array("code" => 400, "message" => "Nom de dossier invalide");
+    $data->new_name = trim($data->new_name);
+    if ($data->new_name == '.' OR $data->new_name == '..') return array("code" => 400, "message" => "Nom de dossier invalide");
     if (!preg_match('/^[a-zA-Z0-9 ._@àâäéèêëïîôöùûüÿç]+$/', $data->new_name)) return array("code" => 400, "message" => "Nom de dossier invalide");
 
     $authorize = $this->conn->prepare("SELECT * FROM `" . $data->db . "`.authorizations WHERE email = :email AND (resource = 'dossiers' OR resource = 'dossiers." . $data->id . "') AND `write` = 1");
@@ -85,7 +86,6 @@ class dossier
     if ($authorize->rowCount() == 0)
       return array("code" => 403, "message" => "Vous n'avez pas les autorisations suffisantes pour effectuer cette action");
 
-    $data->new_name = trim($data->new_name);
     $old_name = $this->conn->query("SELECT nom FROM `" . $data->db . "`.dossiers WHERE id = '" . $data->id . "'");
     if ($old_name->rowCount() == 0)
       return array("code" => 404, "message" => "Ce dossier n'existe pas");
