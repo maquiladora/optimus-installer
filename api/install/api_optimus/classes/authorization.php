72536ee528b5b40d7f02072c7c603003d191b73a
Lionel VEST <lionel.vest@adaris.org>
2020-10-17 16:59:53 +0200

    *

--- api/install/api_optimus/classes/authorization.php
+++ api/install/api_optimus/classes/authorization.php
@@ -11,11 +11,11 @@ class authorization
     if (!preg_match("/^[a-z0-9_@.]+$/", $data->db)) return array("code" => 400, "message" => "Base de données invalide");
     if (!preg_match("/^[a-z0-9_.]+$/", $data->resource)) return array("code" => 400, "message" => "Resource invalide");
 
-    $dossier = $this->conn->query("SELECT 'read', 'write', 'create', 'delete' FROM `" . $data->db . "`.authorizations WHERE email = '" . $payload['user']->email . "' AND resource = '" . $data->resource . "'");
-    if ($dossier->rowCount() == 0)
-      return array("code" => 404, "message" => "Ce dossier n'existe pas");
+    $authorization = $this->conn->query("SELECT 'read', 'write', 'create', 'delete' FROM `" . $data->db . "`.authorizations WHERE email = '" . $payload['user']->email . "' AND resource = '" . $data->resource . "'");
+    if ($authorization->rowCount() == 0)
+      return array("code" => 404, "message" => "Aucune autorisation sur cette ressource");
     else
-      return array("code" => 200, "data" => $dossier->fetch(PDO::FETCH_ASSOC));
+      return array("code" => 200, "data" => $authorization->fetch(PDO::FETCH_ASSOC));
   }
 
 
