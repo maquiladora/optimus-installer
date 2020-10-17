5c5f4e6382532c252438fb49e028bbca9191ad23
Lionel VEST <lionel.vest@adaris.org>
2020-10-17 15:12:18 +0200

    *

--- api/install/api_optimus/dossier.php
+++ api/install/api_optimus/dossier.php
@@ -17,7 +17,7 @@ $db = $database->getConnection();
 $dossier = new dossier($db);
 
 if ($_SERVER['REQUEST_METHOD']=='GET')
-  $data = json_decode(urldecode($_GET['db']));
+  $data = json_decode(urldecode($_GET['data']));
 else
   $data = json_decode(file_get_contents("php://input"));
 
