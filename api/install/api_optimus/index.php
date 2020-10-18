7280287ffebcfa59acbbc11170a1f1efba917db0
Lionel VEST <lionel.vest@adaris.org>
2020-10-18 14:39:53 +0200

    *

--- api/install/api_optimus/index.php
+++ api/install/api_optimus/index.php
@@ -18,6 +18,12 @@ else
   die(json_encode(array("code" => 400, "message" => "Base de données invalide")));
 }
 
+if (@!$path[3] OR   if (preg_match("/^[a-z0-9_]+$/", @$path[3]))
+{
+  http_response_code(400);
+  die(json_encode(array("code" => 400, "message" => "Resource invalide")));
+}
+
 if (@$path[4])
 {
   if (preg_match("/^\d+$/", @$path[4]))
@@ -27,11 +33,6 @@ if (@$path[4])
     http_response_code(400);
     die(json_encode(array("code" => 400, "message" => "Identifiant invalide")));
   }
-}
-
-if (@!$path[3] OR   if (preg_match("/^[a-z0-9_]+$/", @$path[3]))
-{
-  die(json_encode(array("code" => 400, "message" => "Resource invalide")));
 }
 
 if ($path[3] == 'contacts' AND @$data->id)
