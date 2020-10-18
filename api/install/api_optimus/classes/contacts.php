cfa3a7b13ef8dd3f69a18add738402b21d98ebdf
Lionel VEST <lionel.vest@adaris.org>
2020-10-18 09:12:09 +0200

    *

--- api/install/api_optimus/classes/contacts.php
+++ api/install/api_optimus/classes/contacts.php
@@ -16,7 +16,7 @@ class contacts
   function list($data,$payload)
   {
     if (!preg_match("/^[a-z0-9_@.]+$/", $data->db)) return array("code" => 400, "message" => "Base de données invalide");
-    if ($data->categorie AND !preg_match("/^\d+$/", $data->categorie)) return array("code" => 400, "message" => "Identifiant de categorie invalide");
+    if (@$data->categorie AND !preg_match("/^\d+$/", $data->categorie)) return array("code" => 400, "message" => "Identifiant de categorie invalide");
 
     $authorizations_contacts = $this->conn->prepare("SELECT `read`, `write`, `create`, `delete` FROM `" . $data->db . "`.authorizations WHERE email = :email AND resource = 'contacts' ORDER BY length(resource) DESC");
     $authorizations_contacts->bindParam(':email', $payload['user']->email);
@@ -25,7 +25,7 @@ class contacts
     if ($authorizations_contacts['read'] == 0)
       return array("code" => 403, "message" => "Vous n'avez pas les autorisations suffisantes pour accéder aux contacts");
 
-    if ($data->$categorie)
+    if (@$data->categorie)
     {
       $contacts = $this->conn->prepare("SELECT * FROM `" . $data->db . "`.contacts AND categorie = :categorie");
       $contacts->bindParam(':categorie', $data->categorie, PDO::PARAM_INT);
@@ -36,7 +36,7 @@ class contacts
 
     $contacts = $contacts->fetch(PDO::FETCH_ASSOC);
     $contacts['authorizations'] = $authorizations_contacts;
-    return array("code" => 200, "data" => $dossier);
+    return array("code" => 200, "data" => $contacts);
   }
 }
 
