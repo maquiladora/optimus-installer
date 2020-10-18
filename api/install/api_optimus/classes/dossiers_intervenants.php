b231655d0fd3225909e0a31b3732f25b3904f6e5
Lionel VEST <lionel.vest@adaris.org>
2020-10-18 10:20:33 +0200

    *

--- api/install/api_optimus/classes/dossiers_intervenants.php
+++ api/install/api_optimus/classes/dossiers_intervenants.php
@@ -44,4 +44,59 @@ class dossier_intervenants
     }
   }
 }
+
+
+class dossier_intervenant
+{
+  private $conn;
+
+  public $id;
+  public $nom;
+  public $numero;
+  public $date_ouverture;
+
+  public function __construct($db)
+  {
+    $this->conn = $db;
+  }
+
+  function create($data,$payload)
+  {
+    if (!preg_match("/^[a-z0-9_@.]+$/", $data->db)) return array("code" => 400, "message" => "Base de données invalide");
+    if (!preg_match("/^\d+$/", $data->dossier)) return array("code" => 400, "message" => "Identifiant de dossier invalide");
+    if ($data->dossier > 0) return array("code" => 400, "message" => "Un identifiant de dossier doit être renseigné");
+    if (!preg_match("/^\d+$/", $data->contact)) return array("code" => 400, "message" => "Identifiant intervenant invalide");
+    if ($data->contact > 0) return array("code" => 400, "message" => "Un identifiant d'intervenant doit être renseigné");
+    if (!preg_match("/^\d+$/", $data->qualite)) return array("code" => 400, "message" => "Identifiant qualite invalide");
+    if ($data->qualite > 0) return array("code" => 400, "message" => "Un identifiant qualite doit être renseigné");
+    if (!preg_match("/^\d+$/", $data->lien)) return array("code" => 400, "message" => "Identifiant lien invalide");
+
+    $authorizations = $this->conn->prepare("SELECT `read`, `write`, `create`, `delete` FROM `" . $data->db . "`.authorizations WHERE email = :email AND resource = 'dossiers'");
+    $authorizations->bindParam(':email', $payload['user']->email);
+    $authorizations->execute();
+    $authorizations = $authorizations->fetch(PDO::FETCH_ASSOC);
+    if ($authorizations['write'] == 0)
+      return array("code" => 403, "message" => "Vous n'avez pas les autorisations suffisantes pour effectuer cette action");
+
+    $this->dossier = $data->dossier;
+    $this->contact = $data->contact;
+    $this->qualite = $data->qualite;
+    $this->lien = $data->qualite OR 0;
+
+    $intervenant = $this->conn->prepare("INSERT INTO `" . $data->db . "`.dossiers_intervenants SET dossier = :dossier, contact = :contact, qualite = :qualite, lien = :lien");
+    $intervenant->bindParam(':dossier', $this->dossier);
+    $intervenant->bindParam(':contact', $this->contact);
+    $intervenant->bindParam(':qualite', $this->qualite);
+    $intervenant->bindParam(':lien', $this->lien);
+    if($intervenant->execute())
+    {
+      $this->id = $this->conn->lastInsertId();
+      return array("code" => 201, "data" => $this);
+    }
+    else
+      return array("code" => 400, "message" => $stmt->errorInfo()[2]);
+  }
+
+
+
 ?>
