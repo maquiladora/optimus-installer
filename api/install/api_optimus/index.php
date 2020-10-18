2d69f3f12aa62c575ed5a09706418bc38b7b3f40
Lionel VEST <lionel.vest@adaris.org>
2020-10-18 14:26:47 +0200

    *

--- api/install/api_optimus/index.php
+++ api/install/api_optimus/index.php
@@ -1,6 +1,6 @@
 <?php
 if ($_SERVER['REQUEST_METHOD']=='GET')
-  $data = json_decode(urldecode($_GET['data']));
+  $data = json_decode(urldecode(@$_GET['data']));
 else
   $data = json_decode(file_get_contents("php://input"));
 
