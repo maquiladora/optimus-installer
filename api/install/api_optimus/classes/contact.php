706a4c6bab8782ca65e99755820482d814f6f454
Lionel VEST <lionel.vest@adaris.org>
2020-10-18 17:49:11 +0200

    *

---
+++ api/install/api_optimus/classes/contact.php
@@ -0,0 +1,89 @@
+<?php
+class contact
+{
+  private $conn;
+
+  public $id;
+  public $nom;
+  public $numero;
+  public $date_ouverture;
+
+  public function __construct($db)
+  {
+    $this->conn = $db;
+  }
+
+
+  function list($data)
+  {
+    $authorizations = $this->conn->prepare("SELECT `read`, `write`, `create`, `delete` FROM `" . $data->db . "`.authorizations WHERE email = :email AND (resource = 'contacts' or resource = 'contacts." . $data->id . "') ORDER BY length(resource) DESC");
+    $authorizations->bindParam(':email', $data->user);
+    $authorizations->execute();
+    $authorizations = $authorizations->fetch(PDO::FETCH_ASSOC);
+    if ($authorizations['read'] == 0)
+      return array("code" => 403, "message" => "Vous n'avez pas les autorisations suffisantes pour accéder à ce contact");
+
+    $contact = $this->conn->query("SELECT * FROM `" . $data->db . "`.contacts WHERE id = " . $data->id);
+    if ($contact->rowCount() == 0)
+      return array("code" => 404, "message" => "Ce contact n'existe pas");
+    else
+    {
+      $contact = $contact->fetch(PDO::FETCH_ASSOC);
+      return array("code" => 200, "data" => $contact, "authorizations" => $authorizations);
+    }
+  }
+
+
+  function create($data,)
+  {
+
+  }
+
+
+  function delete($data,)
+  {
+
+  }
+
+
+  function update($data,)
+  {
+    if (!preg_match("/^[a-z0-9_]+$/", $data->field)) return array("code" => 400, "message" => "Champ invalide");
+
+    $authorizations = $this->conn->prepare("SELECT `read`, `write`, `create`, `delete` FROM `" . $data->db . "`.authorizations WHERE email = :email AND (resource = 'contacts' OR resource = 'contacts." . $data->id . "' OR resource = 'contacts." . $data->id . "." . $data->field . "') ORDER BY length(resource) DESC");
+    $authorizations->bindParam(':email', $data->user);
+    $authorizations->execute();
+    $authorizations = $authorizations->fetch(PDO::FETCH_ASSOC);
+    if ($authorizations['write'] == 0)
+      return array("code" => 403, "message" => "Vous n'avez pas les autorisations suffisantes pour effectuer cette action");
+
+    $exists = $this->conn->query("SELECT id FROM `" . $data->db . "`.contacts WHERE id = " . $data->id);
+    if ($exists->rowCount() == 0)
+      return array("code" => 404, "message" => "Ce contact n'existe pas");
+
+    $field = $this->conn->prepare("SELECT DISTINCT DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = 'contacts' AND COLUMN_NAME = :field");
+    $field->bindParam(':field', $data->field);
+    if($field->execute())
+      $field = $field->fetch();
+    else
+      return array("code" => 400, "message" => $field->errorInfo()[2]);
+
+    $contact = $this->conn->prepare("UPDATE `" . $data->db . "`.contacts SET `" . $data->field . "` = :value WHERE id = " . $data->id);
+    if ($field['DATA_TYPE'] == 'bit' OR $field['DATA_TYPE'] == 'tinyint' OR $field['DATA_TYPE'] == 'smallint' OR $field['DATA_TYPE'] == 'mediumint'  OR $field['DATA_TYPE'] == 'int' OR $field['DATA_TYPE'] == 'bigint')
+    {
+      if ($data->value=='')
+        $contact->bindValue(':value', null, PDO::PARAM_NULL);
+      else
+        $contact->bindParam(':value', $data->value, PDO::PARAM_INT);
+    }
+    else if (($field['DATA_TYPE'] == 'date' OR $field['DATA_TYPE'] == 'datetime') AND $data->value =='')
+      $contact->bindValue(':value', null, PDO::PARAM_NULL);
+    else
+      $contact->bindParam(':value', $data->value, PDO::PARAM_STR);
+
+    if($contact->execute())
+      return array("code" => 200);
+    else
+      return array("code" => 400, "message" => $contact->errorInfo()[2]);
+  }
+  ?>
--- api/install/api_optimus/contacts.php
+++ api/install/api_optimus/contacts.php
@@ -10,30 +10,28 @@ if ($_SERVER['REQUEST_METHOD'] == "OPTIONS") die(http_response_code(200));
 include_once 'config.php';
 include_once 'connect.php';
 include_once 'api_allspark/auth.php';
-include_once 'api_optimus/classes/contacts.php';
+$data->user = $payload['user']->email;
 
 $database = new Database();
 $db = $database->getConnection();
 
-if ($_SERVER['REQUEST_METHOD']=='GET')
-  $data = json_decode(urldecode($_GET['data']));
-else
-  $data = json_decode(file_get_contents("php://input"));
-
 if (@$data->id OR $_SERVER['REQUEST_METHOD']=='PUT')
-  $contacts = new contact($db);
+{
+  include_once 'api_optimus/classes/contact.php';
+  $intervenants = new contact($db);
+}
 else
-  $contacts = new contacts($db);
+{
+  include_once 'api_optimus/classes/contacts.php';
+  $intervenants = new contacts($db);
+}
 
 if ($_SERVER['REQUEST_METHOD']=='GET')
   $result = $contacts->list($data,$payload);
-
 if ($_SERVER['REQUEST_METHOD']=='PUT')
   $result = $contacts->create($data,$payload);
-
 if ($_SERVER['REQUEST_METHOD']=='DELETE')
   $result = $contacts->delete($data,$payload);
-
 if ($_SERVER['REQUEST_METHOD']=='PATCH')
   $result = $contacts->update($data,$payload);
 
