32ed81aa75e4739c93f9c254a09730b403466159
Lionel VEST <lionel.vest@adaris.org>
2020-10-18 14:47:58 +0200

    *

--- api/install/api_optimus/index.php
+++ api/install/api_optimus/index.php
@@ -16,7 +16,6 @@ else
 {
   http_response_code(400);
   die(json_encode(array("code" => 400, "message" => "Base de données invalide")));
-  }
 }
 
 if (@!$path[3] OR !preg_match("/^[a-z0-9_]+$/", @$path[3]))
