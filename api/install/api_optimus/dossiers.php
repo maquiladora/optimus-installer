870d0c4ff76fc100c3691e3431522cf6e666f9ec
Lionel VEST <lionel.vest@adaris.org>
2020-10-18 14:22:40 +0200

    *

--- api/install/api_optimus/dossiers.php
+++ api/install/api_optimus/dossiers.php
@@ -10,46 +10,29 @@ if ($_SERVER['REQUEST_METHOD'] == "OPTIONS") die(http_response_code(200));
 include_once 'config.php';
 include_once 'connect.php';
 include_once 'api_allspark/auth.php';
-include_once 'api_optimus/classes/dossiers.php';
 
 $database = new Database();
 $db = $database->getConnection();
 
-
-if ($_SERVER['REQUEST_METHOD']=='GET')
-  $data = json_decode(urldecode($_GET['data']));
-else
-  $data = json_decode(file_get_contents("php://input"));
-
-
-if (@$path[3])
-  if (!is_int(@$path[3]))
-  {
-    http_response_code(400);
-    die(json_encode(array("code" => 400, "message" => "Identifiant non valude")));
-  }
-  else
-    $data->id = intval($path[3]);
-
-
 if ($data->id OR $_SERVER['REQUEST_METHOD']=='PUT')
+{
+  include_once 'api_optimus/classes/dossier.php';
   $dossiers = new dossier($db);
+}
 else
+{
+  include_once 'api_optimus/classes/dossiers.php';
   $dossiers = new dossiers($db);
-
+}
 
 if ($_SERVER['REQUEST_METHOD']=='GET')
   $result = $dossiers->list($data,$payload);
-
 if ($_SERVER['REQUEST_METHOD']=='PUT')
   $result = $dossiers->create($data,$payload);
-
 if ($_SERVER['REQUEST_METHOD']=='MOVE')
   $result = $dossiers->rename($data,$payload);
-
 if ($_SERVER['REQUEST_METHOD']=='DELETE')
   $result = $dossiers->delete($data,$payload);
-
 if ($_SERVER['REQUEST_METHOD']=='PATCH')
   $result = $dossiers->update($data,$payload);
 
--- api/install/api_optimus/index.php
+++ api/install/api_optimus/index.php
@@ -1,8 +1,46 @@
 <?php
-if ($path[2] == 'contacts')
+if ($_SERVER['REQUEST_METHOD']=='GET')
+  $data = json_decode(urldecode($_GET['data']));
+else
+  $data = json_decode(file_get_contents("php://input"));
+
+
+if (@$path[2])
+{
+  if (preg_match("/^[a-z0-9_@.]+$/", @$path[2]))
+    $data->db = $path[2];
+}
+else
+{
+  http_response_code(400);
+  die(json_encode(array("code" => 400, "message" => "Base de données invalide")));
+}
+
+
+if (@$path[4])
+{
+  if (preg_match("/^\d+$/", @$path[4]))
+    $data->id = intval($path[4]);
+  else
+  {
+    http_response_code(400);
+    die(json_encode(array("code" => 400, "message" => "Identifiant invalide")));
+  }
+}
+
+
+if ($path[3] == 'contacts' AND $data->id)
+  include_once 'contact.php';
+else if ($path[3] == 'contacts')
   include_once 'contacts.php';
-else if ($path[2] == 'dossiers')
-  include_once 'dossiers.php';
-else if ($path[2] == 'dossiers_intervenants')
+
+if ($path[3] == 'dossiers' AND $data->id)
+  include_once 'dossier.php';
+else if ($path[3] == 'dossiers')
+  include_once 'dossier.php';
+
+if ($path[3] == 'dossiers_intervenants' AND $data->id)
+  include_once 'dossiers_intervenant.php';
+else if ($path[3] == 'dossiers_intervenants')
   include_once 'dossiers_intervenants.php';
 ?>
