38a31ca9a2be22d0d4a5f7ae5d9abead6c8d2482
Lionel VEST <lionel.vest@adaris.org>
2020-10-18 20:00:54 +0200

    *

--- api/install/api_optimus/classes/dossiers_intervenants.php
+++ api/install/api_optimus/classes/dossiers_intervenants.php
@@ -16,18 +16,17 @@ class dossiers_intervenants
 
   function list($data)
   {
-    if (!preg_match("/^[a-z0-9_@.]+$/", $data->db)) return array("code" => 400, "message" => "Base de données invalide");
     if (!preg_match("/^\d+$/", $data->dossier)) return array("code" => 400, "message" => "Identifiant de dossier invalide");
 
     $authorizations_dossier = $this->conn->prepare("SELECT `read`, `write`, `create`, `delete` FROM `" . $data->db . "`.authorizations WHERE email = :email AND (resource = 'dossiers' OR resource = 'dossiers." . $data->dossier . "') ORDER BY length(resource) DESC");
-    $authorizations_dossier->bindParam(':email', $payload['user']->email);
+    $authorizations_dossier->bindParam(':email', $data->user, PDO::PARAM_STR);
     $authorizations_dossier->execute();
     $authorizations_dossier = $authorizations_dossier->fetch(PDO::FETCH_ASSOC);
     if ($authorizations_dossier['read'] == 0)
       return array("code" => 403, "message" => "Vous n'avez pas les autorisations suffisantes pour accéder à ce dossier");
 
     $authorizations_contacts = $this->conn->prepare("SELECT `read`, `write`, `create`, `delete` FROM `" . $data->db . "`.authorizations WHERE email = :email AND resource = 'contacts' ORDER BY length(resource) DESC");
-    $authorizations_contacts->bindParam(':email', $payload['user']->email);
+    $authorizations_contacts->bindParam(':email', $data->user, PDO::PARAM_STR);
     $authorizations_contacts->execute();
     $authorizations_contacts = $authorizations_contacts->fetch(PDO::FETCH_ASSOC);
     if ($authorizations_contacts['read'] == 0)
