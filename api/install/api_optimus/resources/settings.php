c57ef03661cb134d3c9630d71d90820bb02bd553
Lionel VEST <lionel.vest@adaris.org>
2020-10-20 00:35:42 +0200

    *

--- api/install/api_optimus/resources/settings.php
+++ api/install/api_optimus/resources/settings.php
@@ -1,27 +1,27 @@
 <?php
-function read($data)
+function read($db,$data)
 {
   $settings = $db->query("SELECT * FROM `" . $data->db . "`.settings WHERE id LIKE 'dossiers.%'");
   $settings = $settings->fetch(PDO::FETCH_ASSOC);
   return array("code" => 200, "data" => $settings);
 }
 
-function create($data)
+function create($db,$data)
 {
   return array("code" => 501, "message" => 'Méthode non implémentée');
 }
 
-function replace($data)
+function replace($db,$data)
 {
   return array("code" => 501, "message" => 'Méthode non implémentée');
 }
 
-function update($data)
+function update($db,$data)
 {
   return array("code" => 501, "message" => 'Méthode non implémentée');
 }
 
-function delete($data)
+function delete($db,$data)
 {
   return array("code" => 501, "message" => 'Méthode non implémentée');
 }
