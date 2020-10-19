1a7c6ddf36e0e13ca0076b7ce5bce7e06c0fd67e
Lionel VEST <lionel.vest@adaris.org>
2020-10-20 01:10:35 +0200

    *

--- api/install/api_optimus/resources/settings.php
+++ api/install/api_optimus/resources/settings.php
@@ -1,8 +1,8 @@
 <?php
 function read($db,$data)
 {
-  $settings = $db->prepare("SELECT * FROM `" . $data->db . "`.settings WHERE id LIKE :module");
-  $settings->bindParam(':module', $data->module.'.%', PDO::PARAM_STR);
+  $settings = $db->prepare("SELECT * FROM `" . $data->db . "`.settings WHERE id LIKE 'dossiers.%'");
+  //$settings->bindParam(':module', $data->module.'.%', PDO::PARAM_STR);
   $settings->execute();
   while ($setting = $settings->fetch(PDO::FETCH_ASSOC))
     $results[$setting['id']] = $setting['value'];
