bc7b4b29160a6d2f2b624ad481b0201b958c5c72
Lionel VEST <lionel.vest@adaris.org>
2020-10-21 00:38:17 +0200

    *

--- api/install/api_optimus/datagrid.php
+++ api/install/api_optimus/datagrid.php
@@ -44,7 +44,7 @@ function datagrid_query($data,$dblink)
   	$query .= ' AND (';
   	foreach ($data->columns as $key => $column)
   		if ($column->dblink == null)
-  			$query .= $column->field . " LIKE '%" . addslashes($data->global_search) . "%' OR ";
+  			$query .= $column->field . " LIKE '%" . $data->global_search . "%' OR ";
   		else
   		{
   			unset($rowsearch);
