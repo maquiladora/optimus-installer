7d9d60f11e80fc8ebb1261d4a87ac4c43b232e05
Lionel VEST <lionel.vest@adaris.org>
2020-10-20 14:41:41 +0200

    *

--- api/install/api_optimus/resources/dossiers.php
+++ api/install/api_optimus/resources/dossiers.php
@@ -54,7 +54,8 @@ function datagrid_request($data,$db)
   		else
   		{
   			unset($rowsearch);
-  			foreach (${(string)$column->dblink} as $key => $value)
+        $domaines[85] = 'Recouvrements';
+  			foreach (${$column->dblink} as $key => $value)
   				if (preg_match("/" . $data->global_search . "/i", $value))
   					@$rowsearch[] = (is_numeric($key))? $key : "'".$key."'";
   				if (is_array(@$rowsearch))
