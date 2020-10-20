231e21477d8ae4cb0262cc96039f18e28c7b6b4c
Lionel VEST <lionel.vest@adaris.org>
2020-10-20 11:41:10 +0200

    *

--- api/install/api_optimus/resources/dossiers.php
+++ api/install/api_optimus/resources/dossiers.php
@@ -15,13 +15,21 @@ class dossiers
 
   function read($data)
   {
-    //200 OK
-    //404 NOT FOUND
-    //405 NOT ALLOWED
-    //409 CONFLICT
-    //501 NOT IMPLEMENTED
-    //return array("code" => int, "message" => text, "data" => array, "authorizations" => array);
-    return array("code" => 501, "message" => 'Méthode non implémentée');
+    $authorizations = $this->conn->prepare("SELECT `read`, `write`, `create`, `delete` FROM `" . $data->db . "`.authorizations WHERE email = :email AND resource = 'dossiers' ORDER BY length(resource) DESC");
+    $authorizations->bindParam(':email', $data->user, PDO::PARAM_STR);
+    $authorizations->execute();
+    $authorizations = $authorizations_contacts->fetch(PDO::FETCH_ASSOC);
+    if ($authorizations['read'] == 0)
+      return array("code" => 403, "message" => "Vous n'avez pas les autorisations suffisantes pour accéder aux dossiers");
+
+    $dossiers = $this->conn->prepare("SELECT id, numero, nom, rg, date_ouverture, date_classement, numero_archive, domaine, CONCAT(domaine,"-",sous_domaine), conseil, aj, id, id FROM `" . $data->db . "`.contacts");
+    if($dossiers->execute())
+    {
+      $dossiers = $dossiers->fetchAll(PDO::FETCH_ASSOC);
+      return array("code" => 200, "data" => $dossiers, 'authorizations' => $authorizations);
+    }
+    else
+      return array("code" => 400, "message" => $dossiers->errorInfo()[2]);
   }
 
   function create($data)
