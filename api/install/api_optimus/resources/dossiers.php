2aaf8d0a36c118931898eb5f71132e57271e21be
Lionel VEST <lionel.vest@adaris.org>
2020-10-20 11:52:22 +0200

    *

--- api/install/api_optimus/resources/dossiers.php
+++ api/install/api_optimus/resources/dossiers.php
@@ -4,7 +4,7 @@ function read($db,$data)
   $authorizations = $db->prepare("SELECT `read`, `write`, `create`, `delete` FROM `" . $data->db . "`.authorizations WHERE email = :email AND resource = 'dossiers' ORDER BY length(resource) DESC");
   $authorizations->bindParam(':email', $data->user, PDO::PARAM_STR);
   $authorizations->execute();
-  $authorizations = $authorizations_contacts->fetch(PDO::FETCH_ASSOC);
+  $authorizations = $authorizations->fetch(PDO::FETCH_ASSOC);
   if ($authorizations['read'] == 0)
     return array("code" => 403, "message" => "Vous n'avez pas les autorisations suffisantes pour accéder aux dossiers");
   $dossiers = $db->prepare("SELECT id, numero, nom, rg, date_ouverture, date_classement, numero_archive, domaine, CONCAT(domaine,"-",sous_domaine), conseil, aj, id, id FROM `" . $data->db . "`.contacts");
