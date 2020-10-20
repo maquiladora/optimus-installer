7bd80aa63e63bdbc5db913cc45ab041c1276dedc
Lionel VEST <lionel.vest@adaris.org>
2020-10-20 11:50:57 +0200

    *

--- api/install/api_optimus/resources/dossiers.php
+++ api/install/api_optimus/resources/dossiers.php
@@ -1,55 +1,39 @@
 <?php
-class dossiers
+function read($db,$data)
 {
-  private $conn;
-
-  public $id;
-  public $nom;
-  public $numero;
-  public $date_ouverture;
-
-  public function __construct($db)
+  $authorizations = $this->conn->prepare("SELECT `read`, `write`, `create`, `delete` FROM `" . $data->db . "`.authorizations WHERE email = :email AND resource = 'dossiers' ORDER BY length(resource) DESC");
+  $authorizations->bindParam(':email', $data->user, PDO::PARAM_STR);
+  $authorizations->execute();
+  $authorizations = $authorizations_contacts->fetch(PDO::FETCH_ASSOC);
+  if ($authorizations['read'] == 0)
+    return array("code" => 403, "message" => "Vous n'avez pas les autorisations suffisantes pour accéder aux dossiers");
+  $dossiers = $this->conn->prepare("SELECT id, numero, nom, rg, date_ouverture, date_classement, numero_archive, domaine, CONCAT(domaine,"-",sous_domaine), conseil, aj, id, id FROM `" . $data->db . "`.contacts");
+  if($dossiers->execute())
   {
-    $this->conn = $db;
+    $dossiers = $dossiers->fetchAll(PDO::FETCH_ASSOC);
+    return array("code" => 200, "data" => $dossiers, 'authorizations' => $authorizations);
   }
+  else
+    return array("code" => 400, "message" => $dossiers->errorInfo()[2]);
+}
 
-  function read($db,$data)
-  {
-    $authorizations = $this->conn->prepare("SELECT `read`, `write`, `create`, `delete` FROM `" . $data->db . "`.authorizations WHERE email = :email AND resource = 'dossiers' ORDER BY length(resource) DESC");
-    $authorizations->bindParam(':email', $data->user, PDO::PARAM_STR);
-    $authorizations->execute();
-    $authorizations = $authorizations_contacts->fetch(PDO::FETCH_ASSOC);
-    if ($authorizations['read'] == 0)
-      return array("code" => 403, "message" => "Vous n'avez pas les autorisations suffisantes pour accéder aux dossiers");
+function create($db,$data)
+{
+  return array("code" => 501, "message" => 'Méthode non implémentée');
+}
 
-    $dossiers = $this->conn->prepare("SELECT id, numero, nom, rg, date_ouverture, date_classement, numero_archive, domaine, CONCAT(domaine,"-",sous_domaine), conseil, aj, id, id FROM `" . $data->db . "`.contacts");
-    if($dossiers->execute())
-    {
-      $dossiers = $dossiers->fetchAll(PDO::FETCH_ASSOC);
-      return array("code" => 200, "data" => $dossiers, 'authorizations' => $authorizations);
-    }
-    else
-      return array("code" => 400, "message" => $dossiers->errorInfo()[2]);
-  }
+function replace($db,$data)
+{
+  return array("code" => 501, "message" => 'Méthode non implémentée');
+}
 
-  function create($db,$data)
-  {
-    return array("code" => 501, "message" => 'Méthode non implémentée');
-  }
+function update($db,$data)
+{
+  return array("code" => 501, "message" => 'Méthode non implémentée');
+}
 
-  function replace($db,$data)
-  {
-    return array("code" => 501, "message" => 'Méthode non implémentée');
-  }
-
-  function update($db,$data)
-  {
-    return array("code" => 501, "message" => 'Méthode non implémentée');
-  }
-
-  function delete($db,$data)
-  {
-    return array("code" => 501, "message" => 'Méthode non implémentée');
-  }
+function delete($db,$data)
+{
+  return array("code" => 501, "message" => 'Méthode non implémentée');
 }
 ?>
