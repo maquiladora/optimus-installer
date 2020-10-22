c573654fead6b23dfc9e2802f6a39b6e6123af85
Lionel VEST <lionel.vest@adaris.org>
2020-10-22 21:32:08 +0200

    *

---
+++ api/install/api_optimus/resources/ping.php
@@ -0,0 +1,26 @@
+<?php
+function read($db,$data)
+{
+  return array("code" => 200, "message" => 'API OPTIMUS V1.0');
+}
+
+function create($db,$data)
+{
+  return array("code" => 501, "message" => 'Méthode non implémentée');
+}
+
+function replace($db,$data)
+{
+  return array("code" => 501, "message" => 'Méthode non implémentée');
+}
+
+function update($db,$data)
+{
+  return array("code" => 501, "message" => 'Méthode non implémentée');
+}
+
+function delete($db,$data)
+{
+  return array("code" => 501, "message" => 'Méthode non implémentée');
+}
+?>
