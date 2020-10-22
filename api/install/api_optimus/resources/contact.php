bb71df13a9d70a220a32e4f6fdf130646ab5e739
Lionel VEST <lionel.vest@adaris.org>
2020-10-22 23:28:18 +0200

    *

--- api/install/api_optimus/resources/contact.php
+++ api/install/api_optimus/resources/contact.php
@@ -46,7 +46,8 @@ function create($db,$data)
 	$database_fields = $db->query("SELECT DISTINCT COLUMN_NAME, DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'contacts'");
 	while ($database_field = $database_fields->fetch(PDO::FETCH_ASSOC))
 		$fields[$database_field['COLUMN_NAME']] = $database_field['DATA_TYPE'];
-	foreach($data->values as $key => $value)
+	print_r($data);
+	foreach(@$data->values as $key => $value)
 		if (!array_key_exists($key, $fields))
 			return array("code" => 400, "message" => "Le champ " . $field . " n'existe pas dans la table contacts");
 
