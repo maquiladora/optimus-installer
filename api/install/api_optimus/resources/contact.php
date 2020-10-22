8f87a38763683f543624b21cdc757be7a7ed011f
Lionel VEST <lionel.vest@adaris.org>
2020-10-22 23:57:36 +0200

    *

--- api/install/api_optimus/resources/contact.php
+++ api/install/api_optimus/resources/contact.php
@@ -66,14 +66,14 @@ function create($db,$data)
 			if ($fields[$key] == 'bit' OR $fields[$key] == 'tinyint' OR $fields[$key] == 'smallint' OR $fields[$key] == 'mediumint' OR $fields[$key] == 'int' OR $fields[$key] == 'bigint')
 			{
 				if ($value=='')
-					$contact->bindValue(':value', null, PDO::PARAM_NULL);
+					$contact->bindValue(':'.$key, null, PDO::PARAM_NULL);
 				else
-					$contact->bindParam(':value', $value, PDO::PARAM_INT);
+					$contact->bindParam(':'.$key, $value, PDO::PARAM_INT);
 			}
 			else if (($fields[$key] == 'date' OR $fields[$key] == 'datetime') AND $value =='')
-				$contact->bindValue(':value', null, PDO::PARAM_NULL);
+				$contact->bindValue(':'.$key, null, PDO::PARAM_NULL);
 			else
-				$contact->bindParam(':value', $value, PDO::PARAM_STR);
+				$contact->bindParam(':'.$key, $value, PDO::PARAM_STR);
 	}
 	else
 	{
